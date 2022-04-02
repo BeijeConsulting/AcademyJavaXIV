6bc0f87e9e924765db33f08d21f060cdf5b10d98
Lorenzo Leopardi <lorenzo.leopardi95@gmail.com>
2022-04-02 16:40:57 +0200

    adding exercises Morra + Dateprint from Discord

---
+++ Esercizi/src/it/beije/xiv/esercizi/Discord/Dateprint.java
@@ -0,0 +1,31 @@
+package it.beije.exercises;
+import java.time.*;
+import java.time.temporal.IsoFields;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+//Realizzare un programma che, 
+//ricevuta in input una data in 
+//formato 13/09/2021 stampi le 
+//seguenti informazioni:
+//Lunedì 13 Settembre, 
+//giorno 256 dell'anno 2021, 
+//settimana numero 37
+
+public class Dateprint {
+
+	public static void main(String[] args) {
+		
+		DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");		//format for parsing
+		Scanner scan = new Scanner(System.in);  					
+		System.out.println("Enter a date (dd/mm/yyyy) please:");
+		String s1 = scan.nextLine();								//scan for date
+		LocalDate date = LocalDate.parse(s1,f1);					//create LocalDate object by parsing the scanned string
+		scan.close();
+		
+		int weekNumber=date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);	//get week number (complex operation) from IsoFields Class
+		
+		System.out.println((date.getDayOfWeek()+" "+date.getDayOfMonth()+" of "+date.getMonth()+", day "+date.getDayOfYear()+" of the year "+date.getYear()+", week number "+weekNumber+".").toLowerCase());
+	}
+
+}
---
+++ Esercizi/src/it/beije/xiv/esercizi/Discord/Morra.java
@@ -0,0 +1,43 @@
+package it.beije.exercises;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Morra {
+public static void main(String[] args) {
+	
+	ArrayList <String> legalmoves = new ArrayList<>(Arrays.asList("carta", "sasso", "forbice"));
+	
+	Scanner scan = new Scanner(System.in);  					//Move setup
+	System.out.println("Player 1 - Enter your move");
+	String p1 = scan.nextLine(); 
+	System.out.println("Player 2 - Enter your move");
+	String p2 = scan.nextLine(); 
+	scan.close();
+	
+
+	
+	while(true) {												
+	if (!legalmoves.contains((String)p1)|!legalmoves.contains((String)p2)) {
+		System.out.println("illegal input");	//legality check
+		break;
+		}
+	
+	if (p1.equals(p2)) {
+		System.out.println("The match is a draw");		//draw check
+		break;
+	} else if (!p1.equals(legalmoves.get(legalmoves.size()-1))&&p2.equals(legalmoves.get(legalmoves.indexOf(p1)+1))) {
+		System.out.println("Player 1 wins");		//p1 non ultimo - p2 segue p1
+	} else if (!p1.equals(legalmoves.get(0))&&p2.equals(legalmoves.get(legalmoves.indexOf(p1)-1))) {
+		System.out.println("Player 2 wins");		//p1 non primo - p2 precede p1
+	} else if (p1.equals(legalmoves.get(legalmoves.size()-1))&p2.equals(legalmoves.get(0))) {
+		System.out.println("Player 1 wins");		//edge case - p1 ultimo e p2 primo - p2 "segue" p1 
+	} else if (p2.equals(legalmoves.get(legalmoves.size()-1))&p1 .equals(legalmoves.get(0))) {
+		System.out.println("Player 2 wins");		//edge case - p2 ultimo e p1 primo - p2 "precede" p1
+	}
+	break; //exit while (match)	
+}
+
+System.out.println("The match is over");		//game over notification
+}}
+
