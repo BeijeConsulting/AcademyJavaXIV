2469efb66f5474601e3d74ed9ea9d4fcef74dda0
ViiRaLe <mattiapagani97@gmail.com>
2022-04-04 17:16:56 +0200

    added Caffeina class

---
+++ Beije/src/it/beije/xiv/esercizi/Caffeina.java
@@ -0,0 +1,22 @@
+package it.beije.xiv.esercizi;
+
+public class Caffeina
+{
+	public static void main(String[] args)
+	{
+		Caffeina c = new Caffeina();
+		System.out.println(c.caffeina(12));
+	}
+	
+	String caffeina(int n)
+	{
+		if (n < 0) return "N NON POSITIVO";
+		
+		String str = ((n % 3) == 0 && (n % 4) != 0) ? "Java" : "";
+		str += ((n % 3) == 0 && (n % 4) == 0) ? "Coffee" : "";
+		str += ((n % 2) == 0 && !str.equals("")) ? "Script" : "";
+		str += (str.equals("")) ? "match_missed" : "";
+		
+		return str;
+	}
+}
