4a2570558b7e195cc164808f25db0b79b253a5fe
ViiRaLe <mattiapagani97@gmail.com>
2022-04-05 17:14:58 +0200

    rewrote string conversion

--- Beije/src/it/beije/xiv/esercizi/ConvertParenthesis.java
+++ Beije/src/it/beije/xiv/esercizi/ConvertParenthesis.java
@@ -1,52 +1,28 @@
 package it.beije.xiv.esercizi;
 
-import java.util.ArrayList;
-
 public class ConvertParenthesis
 {
-	ArrayList<Character> charList = new ArrayList<>();
-	ArrayList<Character> charListRepeated = new ArrayList<>();
-	
 	public static void main(String[] args)
 	{
 		ConvertParenthesis cp = new ConvertParenthesis();
-		String s = "ABCDEF";
+		String s = "Success";
 		System.out.println(cp.convert(s));
 	}
 	
-	String convert(String s)
+	public String convert(String str)
 	{
-		if (s == null || s.length() == 0) return "ERROR";
+		str = str.toLowerCase();
+		String converted = str;
 		
-		s = s.toLowerCase();
-		initializeList(s);
-		
-		String str = "";
-		
-		for(int i = 0; i < s.length(); i++)
+		for(int i = 0; i < str.length(); i++)
 		{
-			if (isRepeated(charListRepeated, s.charAt(i))) str += ")";
-			else str += "(";
-		}
-		
-		return str;
-	}
-	
-	boolean isRepeated(ArrayList<Character> ls, char c)
-	{
-		return (ls.contains(c)) ? true : false;
-	}
-	
-	void initializeList(String s)
-	{
-		for(int i = 0; i < s.length(); i++)
-		{
-			if (!isRepeated(charList, s.charAt(i)) && !isRepeated(charListRepeated, s.charAt(i))) charList.add(s.charAt(i));
-			else
+			if (str.substring(i+1).contains(str.charAt(i) + ""))
 			{
-				charList.remove((Character)s.charAt(i));
-				charListRepeated.add(s.charAt(i));
+				converted = converted.replace(str.charAt(i), ')');
 			}
+			else converted = converted.replace(str.charAt(i), '(');
 		}
+		
+		return converted;
 	}
 }
