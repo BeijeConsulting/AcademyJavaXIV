c19292b4a33cd6141780650ef82bd79fe0e1620b
Lorenzo Orrù <lorenzoorru999@gmail.com>
2022-04-06 17:18:27 +0200

    Update Camera and Motor

--- cap5_Leopardi_orru_Provezza/device/src/device/Camera.java
+++ cap5_Leopardi_orru_Provezza/device/src/device/Camera.java
@@ -2,4 +2,17 @@ package device;
 
 public interface Camera {
 	//TODO
+	public default void hasCamera() {
+		System.out.println("This device has the camera.");
+	}
+	public void turnOnCamera(boolean x);
+	
+	public void turnOffCamera(boolean x);
+	
+	public void isCameraOn(boolean x);
+	
+	public void cameraQuality(int q);
+	
+	public void changeQuality(int q);
+	
 }
