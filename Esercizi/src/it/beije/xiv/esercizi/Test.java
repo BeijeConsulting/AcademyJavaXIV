4bba4444011cc426361afb63bd8d08f223a4ec03
Ercole24 <ercole24maggio@gmail.com>
2022-04-06 16:48:46 +0200

    Update Test.java

--- Esercizi/src/it/beije/xiv/esercizi/Test.java
+++ Esercizi/src/it/beije/xiv/esercizi/Test.java
@@ -1,9 +1,16 @@
 package it.beije.xiv.esercizi;
 
+import it.beije.xiv.esercizi.cap5CorraroFrontini.interfacce.*;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte.*;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classi.triangoli.*;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classi.treD.*;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classi.quadrilateri.*;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classi.poligoni.*;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Forma f1 = new Quadrato(2.0);
 
 	}
 
