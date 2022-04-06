9af1823c1ce4a60fb10cc9b311523089e505d09e
Isghamor <yosci97@gmail.com>
2022-04-06 16:47:16 +0200

    parte 13

---
+++ Esercizi/src/it/beije/xiv/esercizi/cap5CorraroFrontini/classi/treD/Cono.java
@@ -0,0 +1,31 @@
+package it.beije.xiv.esercizi.cap5CorraroFrontini.classi.treD;
+
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classi.poligoni.Cerchio;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte.SolidiRotazione;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte.Triangolo;
+
+public class Cono extends SolidiRotazione{
+	public Cono(Triangolo figure)
+	{
+		this.figure=figure;
+		this.depth=figure.getAltezza();
+		this.base=new Cerchio();
+	}
+	@Override
+	public double getSurface() {
+		return base.getPerimetro()*depth;
+	}
+
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return base.getArea()*depth;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "cono";
+	}
+
+}
---
+++ Esercizi/src/it/beije/xiv/esercizi/cap5CorraroFrontini/classiastratte/SolidiRotazione.java
@@ -0,0 +1,12 @@
+package it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte;
+
+import it.beije.xiv.esercizi.cap5CorraroFrontini.interfacce.Forma2D;
+import it.beije.xiv.esercizi.cap5CorraroFrontini.interfacce.Forma3D;
+
+public abstract class SolidiRotazione implements Forma3D {
+ protected Forma2D figure;
+ protected double depth;
+ protected Forma2D base;
+ 
+	
+}
