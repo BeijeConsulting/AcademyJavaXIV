9211bcb401cbb86db0d563966c068337004a495c
Ercole24 <ercole24maggio@gmail.com>
2022-04-06 16:35:49 +0200

    pt.11

--- Esercizi/src/it/beije/xiv/esercizi/cap5CorraroFrontini/classi/poligoni/Cerchio.java
+++ Esercizi/src/it/beije/xiv/esercizi/cap5CorraroFrontini/classi/poligoni/Cerchio.java
@@ -5,19 +5,35 @@ import it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte.Poligono;
 public class Cerchio extends Poligono {
 	public final static double PI = 3.14;
 	private double r;
-	public Cerchio(double r) {
-		
+	
+	public Cerchio() {
+		this(0.0);
 	}
+	public Cerchio(double r) {
+		super();
+		this.r = r;
+	}
+	
+	public double getR() {
+		return r;
+	}
+
+	public void setR(double r) {
+		this.r = r;
+	}
+
+	public static double getPi() {
+		return PI;
+	}
+
 	@Override
 	public double getPerimetro() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2*PI*r;
 	}
 
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		return PI*r*r;
 	}
 
 }
---
+++ Esercizi/src/it/beije/xiv/esercizi/cap5CorraroFrontini/classi/poligoni/Pentagono.java
@@ -0,0 +1,19 @@
+package it.beije.xiv.esercizi.cap5CorraroFrontini.classi.poligoni;
+
+import it.beije.xiv.esercizi.cap5CorraroFrontini.classiastratte.Poligono;
+
+public class Pentagono extends Poligono {
+
+	@Override
+	public double getPerimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
