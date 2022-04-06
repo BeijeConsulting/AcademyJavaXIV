8a55edcd227b56337a65799591894af354d4c953
Padawan <Padawan@DESKTOP-5LPU8JM>
2022-04-06 17:21:41 +0200

    updating interfaces

--- cap5_Leopardi_orru_Provezza/device/src/device/TouchScreen.java
+++ cap5_Leopardi_orru_Provezza/device/src/device/TouchScreen.java
@@ -1,5 +1,5 @@
 package device;
 
 public interface TouchScreen extends Screen{
-	//TODO
+	public abstract void hasTouchscreen();		//prints playing game on touchscreen / using touchscreen to focus lens
 }
