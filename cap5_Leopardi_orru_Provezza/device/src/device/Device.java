b7e49580c101fd7bc94ef51688fd5646e5d67293
Padawan <Padawan@DESKTOP-5LPU8JM>
2022-04-06 17:51:18 +0200

    revision to abstract class Device

--- cap5_Leopardi_orru_Provezza/device/src/device/Device.java
+++ cap5_Leopardi_orru_Provezza/device/src/device/Device.java
@@ -1,8 +1,14 @@
 package device;
 
 public abstract class Device {
-	public boolean battery;
+	public boolean battery = false;
+	public boolean isOn = false;
 	public void turnOn() {
+		this.isOn = true;
 		System.out.println("The device is on");
+	}
+	public void turnOff() {
+		this.isOn = false;
+		System.out.println("The device is off");
 	}
 }
