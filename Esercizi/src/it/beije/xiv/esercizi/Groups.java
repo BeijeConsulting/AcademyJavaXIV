00a3ab94b55a6a0328cd61231d288690fdc7c844
alessiomarazzini <102732503+alessiomarazzini@users.noreply.github.com>
2022-04-07 17:02:41 +0200

    Create Groups.java

---
+++ Esercizi/src/it/beije/xiv/esercizi/Groups.java
@@ -0,0 +1,57 @@
+package it.beije.xiv.esercizi;
+
+public class Groups {
+	public static void main(String[]args) {
+		String s = "{}[][()]";
+		System.out.print(groupCheck(s));
+	}
+	public static boolean groupCheck(String s) {
+		StringBuilder s1 = new StringBuilder();
+		for(int i=0;i<s.length();i++) {
+			
+			switch(s.charAt(i)) {
+			
+			case '(':
+				s1.append("(");
+				break;
+				
+			case '[':
+				s1.append('[');
+				break;
+				
+			case '{':
+				s1.append('{');
+				break;
+				
+			case ')':
+				if(s1.toString().endsWith("(")) {
+					s1.deleteCharAt(s1.length()-1);
+					continue;
+				}
+				return false;
+				
+			case ']':
+				if(s1.toString().endsWith("[")) {
+					s1.deleteCharAt(s1.length()-1);
+					continue;
+				}
+				return false;
+				
+			case '}':
+				if(s1.toString().endsWith("{")) {
+					s1.deleteCharAt(s1.length()-1);
+					continue;
+				}
+				return false;
+			
+			default:
+				continue;
+			}
+				
+		}
+		if(s1.length() != 0) {
+			return false;
+		}
+		return true;
+	}
+}
