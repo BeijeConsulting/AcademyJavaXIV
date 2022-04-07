e6d4f849467683461f24f5e76408c947920c3a01
Giuseppe199925 <61455889+Giuseppe199925@users.noreply.github.com>
2022-04-07 15:18:13 +0200

    Add artistic, rhythmic, trampoline method

--- Esercizi/src/it/beije/xiv/esercizi/discipline/ArtisticGymnastic.java
+++ Esercizi/src/it/beije/xiv/esercizi/discipline/ArtisticGymnastic.java
@@ -1,5 +1,47 @@
 package it.beije.xiv.esercizi.discipline;
 
-public class ArtisticGymnastic {
+import it.beije.xiv.esercizi.giochiOlimpici.SummerOlympicGame;
+import it.beije.xiv.esercizi.sport.Gymnastic;
 
+public class ArtisticGymnastic extends SummerOlympicGame implements Gymnastic {
+
+    @Override
+    public boolean checkEquipment() {
+        return true;
+    }
+
+    @Override
+    public String equipmentList() {
+        return "Rings, Pommel horse, symmetrical parallels, bar";
+    }
+
+    @Override
+    public int getNumberOfPlayers() {
+        return 10;
+    }
+
+    @Override
+    public void setNumberOfPlayers(int numberOfPlayers) {
+    this.numberOfPlayers=numberOfPlayers;
+    }
+
+    @Override
+    public boolean checkReady() {
+        return true;
+    }
+
+    @Override
+    public void checkWinner() {
+    System.out.println("The winner is Peppinho");
+    }
+
+    @Override
+    public boolean checkNewWorldRecord() {
+        return true;
+    }
+
+    @Override
+    public boolean dopingTest() {
+        return true;
+    }
 }
--- Esercizi/src/it/beije/xiv/esercizi/discipline/RhythmicGymnastic.java
+++ Esercizi/src/it/beije/xiv/esercizi/discipline/RhythmicGymnastic.java
@@ -1,5 +1,47 @@
 package it.beije.xiv.esercizi.discipline;
 
-public class RhythmicGymnastic {
+import it.beije.xiv.esercizi.giochiOlimpici.SummerOlympicGame;
+import it.beije.xiv.esercizi.sport.Gymnastic;
 
+public class RhythmicGymnastic extends SummerOlympicGame implements Gymnastic {
+
+    @Override
+    public boolean checkEquipment() {
+        return true;
+    }
+
+    @Override
+    public String equipmentList() {
+        return "Rope, hoop, ball, clubs and ribbon";
+    }
+
+    @Override
+    public int getNumberOfPlayers() {
+        return 5;
+    }
+
+    @Override
+    public void setNumberOfPlayers(int numberOfPlayers) {
+        this.numberOfPlayers=numberOfPlayers;
+    }
+
+    @Override
+    public boolean checkReady() {
+        return true;
+    }
+
+    @Override
+    public void checkWinner() {
+        System.out.println("The winner is Peppinho");
+    }
+
+    @Override
+    public boolean checkNewWorldRecord() {
+        return true;
+    }
+
+    @Override
+    public boolean dopingTest() {
+        return true;
+    }
 }
