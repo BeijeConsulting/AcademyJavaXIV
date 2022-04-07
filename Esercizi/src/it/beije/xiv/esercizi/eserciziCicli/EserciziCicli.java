3266ff7e6fcc1f8d64811db33bafdb67910dfee0
Isghamor <yosci97@gmail.com>
2022-04-07 17:44:44 +0200

    completamento

---
+++ Esercizi/src/it/beije/xiv/esercizi/eserciziCicli/EserciziCicli.java
@@ -0,0 +1,102 @@
+package src.it.beije.xiv.esercizi.eserciziCicli;
+
+public class EserciziCicli {
+public static void main(String...args)
+{
+	System.out.print("I primi dieci numeri interi sono :");
+	stampaPrimi10Numeri();
+	System.out.println();
+	System.out.print("I primi dieci numeri interi pari tra 0 e 20 sono :");
+	stampaPrimi10NumeriPari();
+	System.out.println();
+	int n = 6;
+	System.out.print("La tebellina del "+n+" è:");
+	stampaTabellina(n);
+	System.out.println();
+	piramideDiCaratteri(n);
+	stampaTriangolo();
+	System.out.print("I primi cento numeri di Fibonacci sono :");
+	fibonacci(100);
+	System.out.println();
+	System.out.print("le prime "+n+" numeri di Fibonacci sono :");
+	fibonacci(5);
+	System.out.println();
+}
+private static void stampaPrimi10Numeri() {
+	for(int i=0;i<10;i++)
+	{
+		System.out.print(" "+i);
+	}
+}
+private static void stampaPrimi10NumeriPari() {
+	for(int i=20;i>0;i-=2)
+	{
+		System.out.print(" "+i);
+	}
+}
+private static void stampaTabellina(int n) {
+	for(int i=1;i<11;i++)
+	{
+		System.out.print(" "+i*n);
+	}
+}
+private static void piramideDiCaratteri(int n) {
+	for(int i=1;i<n;i++)
+	{
+		for(int k=0;k<i;k++)
+		{
+		System.out.print("#");
+		}
+		System.out.println();
+	}
+	for(int i=1;i<n;i++)
+	{
+		for(int k=n;k>i;k--)
+		{
+		System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+private static void stampaTriangolo()
+{
+	StringBuilder q;
+	StringBuilder uno=new StringBuilder();
+	StringBuilder due = new StringBuilder("7654321");
+	for(int i=1;i<7;i++)
+	{
+	//	q = new StringBuilder();
+		uno.append(i);
+		due.deleteCharAt(0);
+		System.out.println(uno.toString()+" "+due.toString());
+	}
+}
+private static void fibonacci(int n)
+{
+	int precedente1=0,precedente2=0;
+	for(int i=0;i<n;i++)
+	{
+		
+		int temp;
+		System.out.print(" "+(temp=precedente1+precedente2));
+		if (i==0)
+		{
+			precedente1=1;
+		}
+		precedente2=precedente1;
+		precedente1=temp;
+		if (i==0)
+		{
+			precedente2=1;
+		}
+	}
+}
+private static void fibonacciRighe(int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		fibonacci(i+1);
+	}
+}
+}
+
---
+++ Esercizi/src/it/beije/xiv/esercizi/eserciziStringhe/EserciziStringhe.java
@@ -0,0 +1,91 @@
+package src.it.beije.xiv.esercizi.eserciziStringhe;
+
+public class EserciziStringhe {
+	public static void main(String[] args)
+	{
+		String s="aUdhccwiOadcwhfia";
+		System.out.println("Stringa di partenza: "+s);
+		System.out.println("le Vocali sono: "+soloVocali(s));
+		System.out.println("Le maiuscole sono: "+soloMaiusc(s));
+		System.out.println("nella stringa ci sono "+contaLetteraC(s)+" C");
+		System.out.println("al contrario è: "+contrario(s));
+		System.out.println("*******************");
+		String s2= "Treno",s3="Camion",s4="Aereo";
+		System.out.println("date le parole " +s2+", " +s3+" e "+s4);
+		System.out.println("concatenate: "+concatena(s2,s3,s4));
+		System.out.println();
+		System.out.println("un metodo getter per "+s2+" è:");
+		getter(s2);
+		System.out.println();
+		System.out.println("un metodo setter per "+s4+" è:");
+		setter(s4);
+		
+	}
+	
+	private static String soloVocali(String s)
+	{
+		StringBuilder q = new StringBuilder();
+		for(int i=0;i<s.length();i++)
+		{
+			if("aeiou".contains(s.substring(i,i+1)))
+					{
+						q.append(s.charAt(i));
+					}
+		}
+		return q.toString();
+	}
+	private static String soloMaiusc(String s)
+	{
+		StringBuilder q = new StringBuilder();
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)>='A'&&s.charAt(i)<='Z')
+					{
+						q.append(s.charAt(i));
+					}
+		}
+		return q.toString();
+	}	
+	private static int contaLetteraC(String s)
+	{
+		int count=0;
+		int index=-1;
+		do
+		{
+			index++;
+			index=s.indexOf('c',index);
+			if(index!=-1)
+			{
+				count++;
+				
+			}
+			
+		}while(index != -1);
+		return count;
+	}
+	private static String contrario(String s) 
+	{
+		StringBuilder b = new StringBuilder(s);
+		b.reverse();
+		return b.toString();
+	}
+	private static String concatena(String s,String s2,String s3)
+	{
+		String concatenata = s+"*"+s2+"*"+s3;
+		return concatenata;
+	}
+	private static void getter(String s)
+	{
+		System.out.println("public Object get"+s+"()");
+		System.out.println("{");
+		System.out.println("return this."+s+";");
+		System.out.println("}");
+	}
+	private static void setter(String s)
+	{
+		System.out.println("public void set"+s+"(Object o)");
+		System.out.println("{");
+		System.out.println("this."+s+"=o;");
+		System.out.println("}");
+	}
+}
