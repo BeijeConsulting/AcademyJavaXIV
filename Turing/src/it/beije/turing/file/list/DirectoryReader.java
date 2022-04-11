5686a6ad53b580f435b0d27944d18781d461be07
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-04-11 17:57:53 +0200

    DirectoryReader

---
+++ Turing/src/it/beije/turing/file/list/DirectoryReader.java
@@ -0,0 +1,69 @@
+package it.beije.turing.file.list;
+
+import java.io.*;
+
+/**
+ * @author Giuseppe Raddato
+ * Data: 11 apr 2022
+ */
+public class DirectoryReader {
+    private static StringBuilder result= new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        String path="/Users/giusepperaddato/Desktop/ProvaCart";
+        File file= new File(path);
+
+        StringBuilder resultFinal = showDirectoryTree(0, file);
+        writeOnFile(resultFinal, path ,"resultTree");
+
+    }
+
+    private static void writeOnFile(StringBuilder resultFinal, String path,String nomeFile) {
+        File file = new File(path+"/"+nomeFile+".txt");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+            fileWriter.write(resultFinal.toString());
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException fEx) {
+                fEx.printStackTrace();
+            }
+            System.out.println("Il file: "+ file.getName()+" è stato scritto con successo nella cartella "+file.getAbsolutePath());
+        }
+    }
+
+    static StringBuilder showDirectoryTree(int lvl, File file) throws IOException {
+            File[] files = file.listFiles();
+            if(files!=null){
+                for (int i = 0; i < files.length; i++){
+                    if(files[i].isDirectory()){
+                        for (int j = 0; j <lvl; j++) {
+                            System.out.print('\t');
+                            result.append('\t');
+                        }
+
+                        System.out.println(files[i].getName()+"(dir) PATH:"+ file.getAbsolutePath());
+                        result.append(files[i].getName()+"(dir) PATH:"+ file.getAbsolutePath()+"\n");
+
+                       showDirectoryTree(++lvl,files[i]);
+                    }else {
+                        for (int j = 0; j < lvl; j++) {
+                            System.out.print('\t');
+                            result.append("\t");
+                        }
+                        System.out.println(files[i].getName());
+                        result.append(files[i].getName()+"\n");
+                    }
+                }
+            }
+            return result;
+
+    }
+
+
+}
