f705808ba5d347220e3d73788123e90f8fcb5de1
Lorenzo Orrù <lorenzoorru999@gmail.com>
2022-04-11 15:10:24 +0200

    Fix class name

---
+++ Turing/src/it/beije/turing/file/CSVreader.java
@@ -0,0 +1,13 @@
+package it.beije.turing.file;
+
+import java.io.File;
+
+public class CSVreader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("/Users/lorenzoorru0/Desktop/corso_java");
+		System.out.println(file.isDirectory());
+	}
+
+}
