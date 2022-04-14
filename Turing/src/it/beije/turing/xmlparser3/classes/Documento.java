96fe0d6e64d01e9f0001ae6f813bd7ce0c447cee
alessiomarazzini <102732503+alessiomarazzini@users.noreply.github.com>
2022-04-14 11:42:19 +0200

    commit

--- Turing/src/it/beije/turing/xmlparser3/classes/Documento.java
+++ Turing/src/it/beije/turing/xmlparser3/classes/Documento.java
@@ -1,5 +1,11 @@
 package it.beije.turing.xmlparser3.classes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import it.beije.turing.xmlparser3.interfaces.LoadFile;
 
 /**
@@ -20,6 +26,26 @@ public class Documento implements LoadFile {
 
     @Override
     public Documento parse(String path) {
+    	StringBuilder s = new StringBuilder();
+    	try {
+			FileReader fileReader = new FileReader(new File(path));
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while(bufferedReader.ready()) {
+				char row = (char) bufferedReader.read();
+				if(row!='\n'|| row!='\t') {
+					s.append(row);
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
         return null;
     }
 
