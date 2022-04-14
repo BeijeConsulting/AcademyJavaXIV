67434f7dca04dc75d29422ff50bdb42f088ac922
Lorenzo Orrù <lorenzoorru999@gmail.com>
2022-04-14 14:44:49 +0200

    Update document

--- Turing/src/it/beije/turing/xmlparser2/Document.java
+++ Turing/src/it/beije/turing/xmlparser2/Document.java
@@ -5,12 +5,29 @@ import java.util.ArrayList;
 
 public class Document {
 	ArrayList<StringBuilder> content = new ArrayList<StringBuilder>();
+	Element root = new Element();
 	
 	public Document(StringBuilder fileContent) {
 		StringBuilder strSup = new StringBuilder();
+		StringBuilder strC = new StringBuilder();
 		
-		for(int i = 0, j = 0, x = 0; i < fileContent.length(); i++) {
+		for(int i = 0, j = 0, x = 0, y = 0; i < fileContent.length(); i++) {
 			char c = fileContent.charAt(i);
+			
+			//System.out.println(strSup);
+			
+			if(i > 2 && fileContent.charAt(i - 1) == '>' && c != '<' && c != '>') {
+				y++;
+			}
+			if(y == 1 && c != '<') {
+				strC.append(c);
+			}
+			
+			if(y == 1 && c == '<') {
+				y--;
+				content.add(strC);
+				strC = new StringBuilder();
+			}
 			
 			if(c == '<' && fileContent.charAt(i + 1) != '?') {
 				j++;
@@ -36,9 +53,9 @@ public class Document {
 				content.add(strSup);
 				strSup = new StringBuilder();
 			} 
-			
-			
 		}
+		
+		
 	}
 	
 	public static void main(String... args) throws IOException {
--- Turing/src/it/beije/turing/xmlparser2/Element.java
+++ Turing/src/it/beije/turing/xmlparser2/Element.java
@@ -1,5 +1,5 @@
 package it.beije.turing.xmlparser2;
 
 public class Element {
-
+	
 }
