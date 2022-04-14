961e74fcd778e8c070d15b9f51f8cee61044fbc0
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-04-14 09:18:20 +0200

    DirectoryReader

---
+++ Turing/src/it/beije/turing/MainParser.java
@@ -0,0 +1,11 @@
+package it.beije.turing;
+
+/**
+ * @author Giuseppe Raddato
+ * Data: 14 apr 2022
+ */
+public class MainParser {
+    public static void main(String[] args) {
+        System.out.println("Hello Parser!!!");
+    }
+}
