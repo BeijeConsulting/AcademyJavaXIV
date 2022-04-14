71bb3879cb15689d251942d9ba1fb0b64f027f62
GinoGreen <luigiverde97@gmail.com>
2022-04-14 11:51:52 +0200

    Merge branch 'main' of git@github.com:BeijeConsulting/AcademyJavaXIV.git into main

--- Turing/src/it/beije/turing/xmlparser5/AlberaturaXML.java
+++ Turing/src/it/beije/turing/xmlparser5/AlberaturaXML.java
@@ -1,5 +1,8 @@
 package it.beije.turing.xmlparser5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlberaturaXML {
 	
 //  getPrimaRiga();
@@ -11,5 +14,36 @@ public class AlberaturaXML {
 //	getTextContent() //torna il contenuto del tag
 //	getAttributes() //torna l'elenco degli attributi dell'elemento
 //	getAttribute(String attribute) //torna il valore dell'attributo specificato
-
+	
+	public List<String> getChildElements(String el) {
+		List<String> children = new ArrayList<>();
+		int indexBegin = el.indexOf('>') + 1;
+		int indexEnd = el.lastIndexOf('<');
+		String textContent = el.substring(indexBegin, indexEnd);
+		String tagName = null;
+		int firstSpacebar = textContent.indexOf(" ");
+		int closingTag = textContent.indexOf(">");
+		
+		if ((firstSpacebar != -1) && (closingTag < firstSpacebar)) {
+			tagName = textContent.substring(1, closingTag);
+		} else {
+			tagName = textContent.substring(1, firstSpacebar);
+		}
+		
+		while (textContent.contains(tagName)) {
+			String closureTag = "/" + tagName + ">";
+			int occurrenceOfClosure = textContent.indexOf(closureTag);
+			int endOfClosure = occurrenceOfClosure + closureTag.length() - 1;
+			
+			String child = textContent.substring(0, endOfClosure);
+			
+			textContent = textContent.substring(endOfClosure);
+			
+			children.add(child);
+		}
+		
+		
+		return children;
+	}
+	
 }
