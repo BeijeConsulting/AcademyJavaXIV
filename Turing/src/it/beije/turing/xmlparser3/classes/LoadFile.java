0f6387d69122561bbf1f5d99f2c8ae2f2cdf7b95
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-04-14 09:48:00 +0200

    XML In

---
+++ Turing/src/it/beije/turing/xmlparser3/classes/LoadFile.java
@@ -0,0 +1,14 @@
+package it.beije.turing.xmlparser3.classes;
+
+import it.beije.turing.xmlparser3.interfaces.ILoadFile;
+
+/**
+ * @author Giuseppe Raddato
+ * Data: 14 apr 2022
+ */
+public class LoadFile implements ILoadFile {
+    @Override
+    public Documento parse(String file) {
+        return null;
+    }
+}
