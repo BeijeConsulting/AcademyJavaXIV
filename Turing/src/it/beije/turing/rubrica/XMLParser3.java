520bd835660ca7f4d6de909796fb01ef8c07ec5e
alessiomarazzini <102732503+alessiomarazzini@users.noreply.github.com>
2022-04-14 09:26:39 +0200

    commit

---
+++ Turing/src/it/beije/turing/rubrica/XMLParser3.java
@@ -0,0 +1,16 @@
+package it.beije.turing.xmlparser3;
+
+/**
+ * 
+ * @author Cognome Nome, Cognome Nome
+ *
+ */
+
+public class XMLParser3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
