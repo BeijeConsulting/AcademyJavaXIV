bf1be2aac0cf997dd5c9cb2ec50d543531b4922b
Giuseppe199925 <61455889+Giuseppe199925@users.noreply.github.com>
2022-04-14 12:13:11 +0200

    Modify Node class

--- Turing/src/it/beije/turing/xmlparser4/Node.java
+++ Turing/src/it/beije/turing/xmlparser4/Node.java
@@ -5,13 +5,23 @@ import java.util.List;
 public class Node  {
 
     String name;
-    List <attributs> attributs;
+    List <Attributes> attributes;
     List <Node> children;
+    String content;
 
-    public Node(String name, List <attribute> ) {
+    public Node(String name, List<Attributes> attributes, String content) {
         this.name = name;
-        this.attr
+        this.attributes = attributes;
+        this.content=content;
+    }
 
+    public Node(String name) {
+        this(name, null, null);
+    }
+
+    public Node(String name, String content) {
+        this.name = name;
+        this.content = content;
     }
 
     public void getChildNodes() {
@@ -23,17 +33,14 @@ public class Node  {
 
     }
 
-    @Override
     public void getTagName() {
 
     }
 
-    @Override
     public void getTextContent() {
 
     }
 
-    @Override
     public void getAttributes() {
 
     }
