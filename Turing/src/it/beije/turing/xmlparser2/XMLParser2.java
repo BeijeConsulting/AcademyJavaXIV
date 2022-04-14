87f4c4858a99bf204b00bd718bac5bbba35a2c5a
Marco Nardella <marco.nardella97@gmail.com>
2022-04-14 09:54:10 +0200

    readText xml

--- Turing/src/it/beije/turing/xmlparser2/XMLParser2.java
+++ Turing/src/it/beije/turing/xmlparser2/XMLParser2.java
@@ -2,14 +2,30 @@ package it.beije.turing.xmlparser2;
 
 /**
  * 
- * @author Cognome Nome, Cognome Nome
+ * @author Nardella Marco, Lorenzo Orrù
+ * 	Il parser dovrà leggere e "costruire" in memoria l'alberatura del file XML partendo da un metodo
+ *
+ *	public static VostroOggettoDocumento parse(String file) {...}
+ *
+ *	che restituirà appunto un VostroOggettoDocumento (nome che deciderete voi).
+ *
+ *	I metodi da implementare sono:
+ *
+ *	getRootElement() //torna l'elemento root
+ *	getChildNodes() //torna tutti i nodi "figli" interni all'elemento su cui viene eseguito
+ *	getChildElements() //torna i soli elementi figli dell'elemento su cui viene eseguito
+ *	getElementsByTagName(String tagName) //torna TUTTI gli elementi con quello specifico nome
+ *	getTagName() //torna il nome del tag
+ *	getTextContent() //torna il contenuto del tag
+ *	getAttributes() //torna l'elenco degli attributi dell'elemento
+ *	getAttribute(String attribute) //torna il valore dell'attributo specificato
  *
  */
 public class XMLParser2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ciao
+		
 	}
 
 }
