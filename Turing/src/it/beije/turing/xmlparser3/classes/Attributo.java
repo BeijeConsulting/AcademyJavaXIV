4d69601cff753272cc12d0b4c594d34b3a6ec7dc
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-04-14 11:26:01 +0200

    XML Design

---
+++ Turing/src/it/beije/turing/xmlparser3/classes/Attributo.java
@@ -0,0 +1,28 @@
+package it.beije.turing.xmlparser3.classes;
+
+/**
+ * @author Giuseppe Raddato
+ * Data: 14 apr 2022
+ */
+public class Attributo{
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    private String name;
+    private String value;
+
+}
