f3729a4536952ad7e0a0fcc162c254c32f814dfe
Marco Nardella <marco.nardella97@gmail.com>
2022-04-14 14:45:46 +0200

    Element update

--- Turing/src/it/beije/turing/xmlparser2/Element.java
+++ Turing/src/it/beije/turing/xmlparser2/Element.java
@@ -1,5 +1,36 @@
 package it.beije.turing.xmlparser2;
 
 public class Element {
+	private String tag;
+	private String[] childElements;
+	private String content;
+	
+	public Element() {
+		
+	}
+
+	public String[] getChildElements() {
+		return childElements;
+	}
+
+	public void setChildElements(String[] childElements) {
+		this.childElements = childElements;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
 
 }
