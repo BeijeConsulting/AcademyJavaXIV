ae705adf25589490678c43aae700e599920754c7
ViiRaLe <mattiapagani97@gmail.com>
2022-04-14 11:32:41 +0200

    added XMLDocument and reading logic

--- Turing/src/it/beije/turing/xmlparser6/XMLDocument.java
+++ Turing/src/it/beije/turing/xmlparser6/XMLDocument.java
@@ -2,5 +2,27 @@ package it.beije.turing.xmlparser6;
 
 public class XMLDocument
 {
+	private Prolog prolog;			//intestazione xml. potrebbe non esistere
+	private Elemento root;			//elemento di root del documento xml
 	
+	
+	public void setProlog(Elemento prolog)
+	{
+		this.prolog = prolog;
+	}
+	
+	public void setRoot(Elemento root)
+	{
+		this.root = root;
+	}
+	
+	public Elemento getProlog()
+	{
+		return prolog;
+	}
+	
+	public Elemento getRoot()
+	{
+		return root;
+	}
 }
--- Turing/src/it/beije/turing/xmlparser6/XMLParser6.java
+++ Turing/src/it/beije/turing/xmlparser6/XMLParser6.java
@@ -1,42 +1,75 @@
 package it.beije.turing.xmlparser6;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * 
  * @author Cognome Pagani, Cognome Provezza
  *
  */
 
-
-
-/*
- * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<contatti>
-	<contatto>
-		<nome>Pippo</nome>
-		<cognome>Pluto</cognome>
-		<telefono>3331234567</telefono>
-		<email>pippo@pluto.net</email>
-	</contatto>
-	<contatto>
-		<nome>Paolino</nome>
-		<cognome>Paperino</cognome>
-		<telefono>00423803243423</telefono>
-	</contatto>
-</contatti>
- * 
- * 
- */
-
-
 public class XMLParser6
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException, IllegalArgumentException
 	{
+		Scanner kb = new Scanner(System.in);								//creo tastiera
+		XMLDocument document = parse(readFile(kb.next()));					//parsing del file preso da input utente
 		
+		kb.close();															//chiudo tastiera
 	}
 	
-	public static XMLDocument parse()
+	public static XMLDocument parse(String file)
 	{
+		if (file == null || file.length() == 0) throw new IllegalArgumentException("File non valido.");
+		
+		
+		
 		return null;
+	}
+	
+	static String readFile(String path) throws FileNotFoundException
+	{
+		File file = new File(path);
+		
+		if (!file.exists())
+		{
+			throw new FileNotFoundException("File non trovato.");
+		}
+		
+		FileReader fileReader = new FileReader(file);						//apro reader con file esistente. FileNotFoundException già gestita, ma comunque non possibile.
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		String fileXml = null;
+		
+		try
+		{
+			while (bufferedReader.ready())
+			{
+				fileXml += (bufferedReader.readLine());						//leggo il file
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				bufferedReader.close();											//chiudo il bufferedreader
+				fileReader.close();												//chiudo il reader
+			}
+			catch (IOException e)												//reader exceptions
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return fileXml;
 	}
 }
