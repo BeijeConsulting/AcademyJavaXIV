69794f5070a2f5475a12ba4a389ae6b6cfb8281e
AlePa <alessandro.pasqua02@gmail.com>
2022-04-14 12:40:58 +0200

    fix exception getChild

--- Turing/src/it/beije/turing/xmlparser5/XMLParser5.java
+++ Turing/src/it/beije/turing/xmlparser5/XMLParser5.java
@@ -12,13 +12,14 @@ public class XMLParser5 {
 	public static void main(String[] args) {
 		
 		AlberaturaXML m = new AlberaturaXML();
-		m.setPath("/temp/test_parser1.xml");
+		//m.setPath("/temp/test_parser1.xml");
+		m.setPath("C:\\Users\\aless\\Desktop\\xml scaricati\\test_parser1.xml");
 		String stringaContenutoFile = m.getContenutoFile();
 		//String stringaContenutoFile = m.getContenutoFile("C:\\Users\\aless\\Desktop\\xml scaricati\\provaintestazione.txt");
 		String rootElement = m.getRootElement();
-		if(rootElement.contains("XML formattato male")) {
-			return;
-		}
+//		if(rootElement.contains("XML formattato male")) {
+//			return;
+//		}
 		List<String> children = m.getChildElements();
 	}
 
