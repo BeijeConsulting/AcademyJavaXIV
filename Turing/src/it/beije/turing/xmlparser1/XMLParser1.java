e310ed93510af476fd96f4f1691fa6179f200b23
ivfly <iv0fly@gmail.com>
2022-04-15 18:16:46 +0200

    added testChallenge(...)

--- Turing/src/it/beije/turing/xmlparser1/XMLParser1.java
+++ Turing/src/it/beije/turing/xmlparser1/XMLParser1.java
@@ -17,7 +17,50 @@ import java.util.Scanner;
  */
 public class XMLParser1 {
 
+	public static void printElements(List<Elemento> elements, String tabs) {
+		for (Elemento element : elements) {
+//			System.out.println(tabs + "element : " + element);
+			System.out.println(tabs + "element.getTagName() : " + element.getTagName());
+//			System.out.println(tabs + "element.getTextContent() : " + element.getTextContent());
+			System.out.println(tabs + "element.getChildNodes() : " + element.getChildNodes());
+			List<Elemento> innerElements = element.getChildElements();
+			System.out.println(tabs + "element.getChildElements() : " + innerElements);
+			//getElementsByTagName NON STANDARD E NON DOCUMENTATO
+//			System.out.println(tabs + "element.getElementsByTagName(\"contatto\") : " + element.getElementsByTagName("contatto"));
+//			System.out.println(tabs + "element.getElementsByTagName(\"altro\") : " + element.getElementsByTagName("altro"));
+			System.out.println(tabs + "element.getAttributes() : " + element.getAttributes());
+			System.out.println(tabs + "element.getAttribute(\"name\") : " + element.getAttribute("name"));
+			
+			if (innerElements != null && !innerElements.isEmpty()) {
+				printElements(innerElements, tabs + '\t');
+			}
+		}
+	}
+	
+	public static void testChallenge(String path) {
+		Documento doc = DocumentoBuilder.parse(path);
+
+		Elemento root = doc.getDocumentElement();
+//		System.out.println("root : " + root);
+		System.out.println("root.getTagName() : " + root.getTagName());
+		System.out.println("root.getTextContent() : " + root.getTextContent());
+		System.out.println("root.getChildNodes() : " + root.getChildNodes());
+		List<Elemento> elements = root.getChildElements();
+		System.out.println("root.getChildElements() : " + elements);
+		//getElementsByTagName NON STANDARD E NON DOCUMENTATO
+//		System.out.println("root.getElementsByTagName(\"contatto\") : " + root.getElementsByTagName("contatto"));
+//		System.out.println("root.getElementsByTagName(\"altro\") : " + root.getElementsByTagName("altro"));
+		System.out.println("root.getAttributes() : " + root.getAttributes());
+		System.out.println("root.getAttribute(\"question\") : " + root.getAttribute("question"));
+		
+		printElements(elements, "\t");
+	}
+
 	public static void main(String[] args) {
+		testChallenge("/tmp/test_parser1.xml");
+	}
+	
+	public static void console(String[] args) {
 
 		System.out.println("Per uscire, digitare \"exit\".");
 		System.out.println("Inserisci nome del file .xml: ");
