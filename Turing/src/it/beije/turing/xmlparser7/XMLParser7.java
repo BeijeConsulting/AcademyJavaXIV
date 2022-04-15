d3407d2a8c179c6454dbd2817f4e2dcc5a3dedaf
ivfly <iv0fly@gmail.com>
2022-04-15 17:31:30 +0200

    added testChallenge(...)

--- Turing/src/it/beije/turing/xmlparser7/XMLParser7.java
+++ Turing/src/it/beije/turing/xmlparser7/XMLParser7.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 
@@ -12,14 +13,53 @@ import java.io.IOException;
  */
 public class XMLParser7 {
 
+	public static void printElements(List<Element> elements, String tabs) {
+		for (Element element : elements) {
+//			System.out.println(tabs + "element : " + element);
+			System.out.println(tabs + "element.getTagName() : " + element.getTagName());
+//			System.out.println(tabs + "element.getTextContent() : " + element.getTextContent());
+			//System.out.println(tabs + "element.getChildNodes() : " + element.getChildNodes());
+			List<Element> innerElements = element.getChildElements();
+			System.out.println(tabs + "element.getChildElements() : " + innerElements);
+			System.out.println(tabs + "element.getElementsByTagName(\"contatto\") : " + element.getElementsByTagName("contatto"));
+			System.out.println(tabs + "element.getElementsByTagName(\"altro\") : " + element.getElementsByTagName("altro"));
+			System.out.println(tabs + "element.getAttributes() : " + element.getAttributes());
+			System.out.println(tabs + "element.getAttribute(\"name\") : " + element.getAttribute("name"));
+			
+			if (innerElements != null && !innerElements.isEmpty()) {
+				printElements(innerElements, tabs + '\t');
+			}
+		}
+	}
+	
+	public static void testChallenge(String path) {
+		Documento documento = Documento.parse(path);
+
+		Root root = documento.getRootElement();
+		System.out.println("root : " + root);
+		System.out.println("root.getTagName() : " + root.getTagName());
+		System.out.println("root.getTextContent() : " + root.getTextContent());
+		//System.out.println("root.getChildNodes() : " + root.getChildNodes());
+		List<Element> elements = root.getChildElements();
+		System.out.println("root.getChildElements() : " + elements);
+		System.out.println("root.getElementsByTagName(\"contatto\") : " + root.getElementsByTagName("contatto"));
+		System.out.println("root.getElementsByTagName(\"altro\") : " + root.getElementsByTagName("altro"));
+		System.out.println("root.getAttributes() : " + root.getAttributes());
+		System.out.println("root.getAttribute(\"question\") : " + root.getAttribute("question"));
+		
+		printElements(elements, "\t");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		"/Users/simonepitossi/File/test_parser6.xml"
 		//"C:/Users/luigi/Downloads/test_parser6.xml"
 
 		//Documento documento = Documento.parse("C:/Users/luigi/Downloads/test_parser6.xml");
-		Documento documento = Documento.parse("/Users/simonepitossi/File/test_parser2.xml");
-		documento.getRootElement().toString();
+//		Documento documento = Documento.parse("/Users/simonepitossi/File/test_parser2.xml");
+//		documento.getRootElement().toString();
+		
+		testChallenge("/tmp/test_parser6.xml");
 	}
 
 	/*
