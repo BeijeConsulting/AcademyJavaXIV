55d4729f3c52c4a1a702d7b2bc8242e4f155731b
GinoGreen <luigiverde97@gmail.com>
2022-04-15 11:17:29 +0200

    fix content

--- Turing/src/it/beije/turing/xmlparser5/XMLParser5.java
+++ Turing/src/it/beije/turing/xmlparser5/XMLParser5.java
@@ -12,18 +12,13 @@ public class XMLParser5 {
 	public static void main(String[] args) {
 		
 
-//		DocumentoXML d = parse("/temp/test_parser1.xml");
-		DocumentoXML d = parse("C:\\Users\\aless\\Desktop\\xml scaricati\\test_parser1.xml");
+		DocumentoXML d = parse("/temp/test_parser1.xml");
+//		DocumentoXML d = parse("C:\\Users\\aless\\Desktop\\xml scaricati\\test_parser1.xml");
 //		DocumentoXML d = parse("C:\\Users\\aless\\Desktop\\xml scaricati\\test_parser1 - Copia.xml");
-//		String s = d.removeDeclarationTag();
 		Tag root = d.getRootElement();
-		System.out.println(root.toString());
 		
-		//dobbiamo lavorare con la root.
-		//dalla variabile root dobbiamo chiamare gli altri metodi
-		
-//		Tag doc = d.buildDoc(root);
-//		System.out.println(doc.toString());
+		Tag doc = d.buildDoc(root);
+		System.out.println(doc.toString());
 	}
 
 	private static DocumentoXML parse(String path) {
