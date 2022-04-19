f5632a2d59a6454acde712763f666c79edfd4e78
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-04-19 18:39:36 +0200

    stampa

--- Turing/src/it/beije/turing/myRubrica/MyRubrica.java
+++ Turing/src/it/beije/turing/myRubrica/MyRubrica.java
@@ -12,14 +12,20 @@ public class MyRubrica {
 
     public static void main(String[] args) {
         OpRubrica rubrica= new SQLManager();
-
-       print(rubrica.showContact(Order.NOME)) ;
+        print(rubrica.showContact(Order.NO)) ;
 
 
 
     }
 
-    private static void print(List<Contatto> showContact) {
+    private static void print(List<Contatto> listContact) {
+        System.out.println("====================================================================================================================================================================================");
+        System.out.printf("| %10s | %30s | %30s | %35s | %15s | %30s |\n","ID","NOME","COGNOME","EMAIL","TELEFONO","NOTE");
+        System.out.println("====================================================================================================================================================================================");
+        for (Contatto c:listContact) {
+        System.out.printf("| %10s | %30s | %30s | %35s | %15s | %30s |\n",c.getId(),c.getNome(),c.getCognome(),c.getEmail(),c.getTelefono(),c.getNote());
+        }
+        System.out.println("==========================================================================FINE=====================================================================================================");
 
     }
 
