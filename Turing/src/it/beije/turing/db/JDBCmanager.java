b43b4d2bb535425dc27221114d3ff59102090163
Lorenzo Orrù <lorenzoorru999@gmail.com>
2022-04-20 12:34:47 +0200

    Update JDBCmanager

--- Turing/src/it/beije/turing/db/JDBCmanager.java
+++ Turing/src/it/beije/turing/db/JDBCmanager.java
@@ -84,43 +84,30 @@ public class JDBCmanager {
 	public static void importFromCSV(String path, String separatore, boolean virgolette) throws IOException {
 		List<Contatto> contatti = CSVmanager.loadRubricaFromCSV(path, separatore, virgolette);
 		
-		Connection connection = null;
-		Statement statement = null;
-		
-		try {
-			connection = getConnection();
-			statement = connection.createStatement();
-			
-			for(Contatto contatto : contatti) {
-				statement.executeUpdate("INSERT INTO rubrica VALUES (null, '"+contatto.getNome()+"', '"+contatto.getNome()+"', '"+contatto.getNome()+"', '"+contatto.getNome()+"', "+contatto.getNome()+")");
-			}
-		} catch (ClassNotFoundException cnfEx) {
-			cnfEx.printStackTrace();
-		} catch (SQLException sqlEx) {
-			sqlEx.printStackTrace();
-		} finally {
-			try {
-				statement.close();
-				connection.close();
-			} catch (SQLException sqlEx) {
-				sqlEx.printStackTrace();
-			}
-		}
+		insertContacts(contatti);
 	}
 	
 	public static void importFromXML(String path) throws ParserConfigurationException, IOException, SAXException {
 		List<Contatto> contatti = XMLmanager.loadRubricaFromXML(path);
 		
+		insertContacts(contatti);
+	}
+	
+	public static void insertContacts(List<Contatto> contatti) {
 		Connection connection = null;
-		Statement statement = null;
+		PreparedStatement insertPrepStatement = null;
 		
 		try {
 			connection = getConnection();
-			statement = connection.createStatement();
+			insertPrepStatement = connection.prepareStatement("INSERT INTO rubrica VALUES (null, ?, ?, ?, ?, ?)");
 			
 			for(Contatto contatto : contatti) {
-				System.out.println("INSERT INTO rubrica VALUES (null, '"+contatto.getNome()+"', '"+contatto.getCognome()+"', '"+contatto.getEmail()+"', '"+contatto.getTelefono()+"', '"+contatto.getNote()+"');");
-				statement.executeUpdate("INSERT INTO rubrica VALUES (null, '"+contatto.getNome()+"', '"+contatto.getCognome()+"', '"+contatto.getEmail()+"', '"+contatto.getTelefono()+"', "+contatto.getNote()+");");
+				insertPrepStatement.setString(1, contatto.getNome());
+				insertPrepStatement.setString(2, contatto.getCognome());
+				insertPrepStatement.setString(3, contatto.getEmail());
+				insertPrepStatement.setString(4, contatto.getTelefono());
+				insertPrepStatement.setString(5, contatto.getNote());
+				insertPrepStatement.executeUpdate();
 			}
 		} catch (ClassNotFoundException cnfEx) {
 			cnfEx.printStackTrace();
@@ -128,7 +115,7 @@ public class JDBCmanager {
 			sqlEx.printStackTrace();
 		} finally {
 			try {
-				statement.close();
+				insertPrepStatement.close();
 				connection.close();
 			} catch (SQLException sqlEx) {
 				sqlEx.printStackTrace();
@@ -136,71 +123,22 @@ public class JDBCmanager {
 		}
 	}
 	
- 	public static void main(String[] args) {
-		
-		Connection connection = null;
-		Statement statement = null;
+ 	public static void updateContact(int id, Contatto contatto) {
+ 		Connection connection = null;
 		PreparedStatement insertPrepStatement = null;
-		ResultSet rs = null;
+		
 		try {
 			connection = getConnection();
-			statement = connection.createStatement();
+			insertPrepStatement = connection.prepareStatement("UPDATE rubrica SET nome = ?, cognome = ?, email = ?, telefono = ?, note = ? where id = ?");
 			
-			//INSERT
-
-			statement.executeUpdate("INSERT INTO rubrica VALUES (null, 'Mario', 'Rossi', 'mario.rossi@tim.it', '3337658390', 'compagno di squadra')");
-
-			//statement.executeUpdate("INSERT INTO rubrica VALUES (null, 'Piero', 'Verde', 'l.verde@beije.it', '5355223532', null)");
-			Contatto contatto = new Contatto();
-			contatto.setCognome("Leopardi");
-			contatto.setNome("Giacomo");
-			contatto.setEmail("g.leopardi@poeti.it");
-			statement.executeUpdate("INSERT INTO rubrica VALUES (null, '" + contatto.getNome() + "', '" + contatto.getCognome() + "', '" + contatto.getEmail() + "', '" + contatto.getTelefono() + "', " + contatto.getNote() + ")");
-			insertPrepStatement = connection.prepareStatement("INSERT INTO rubrica VALUES (null, ?, ?, ?, ?, ?)");
+			
 			insertPrepStatement.setString(1, contatto.getNome());
 			insertPrepStatement.setString(2, contatto.getCognome());
 			insertPrepStatement.setString(3, contatto.getEmail());
 			insertPrepStatement.setString(4, contatto.getTelefono());
 			insertPrepStatement.setString(5, contatto.getNote());
+			insertPrepStatement.setInt(6, id);
 			insertPrepStatement.executeUpdate();
-
-			
-			//UPDATE
-			//statement.executeUpdate("UPDATE rubrica SET telefono = '123452' where id = 2");
-
-			//DELETE
-			//statement.executeUpdate("DELETE FROM rubrica where id = 4");
-
-			//SELECT
-//			if (statement.execute("SELECT * FROM rubrica")) {
-//				ResultSet rs = statement.getResultSet(); 
-//			}
-			rs = statement.executeQuery("SELECT * FROM rubrica");			
-			//ResultSet rs = statement.executeQuery("SELECT nome, cognome, email FROM rubrica");
-			while (rs.next()) {
-//				System.out.println("id : " + rs.getInt(1));				
-//				System.out.println("nome : " + rs.getString(2));				
-//				System.out.println("cognome : " + rs.getString(3));				
-//				System.out.println("email : " + rs.getString(4));				
-//				System.out.println("telefono : " + rs.getString(5));				
-//				System.out.println("note : " + rs.getString(6));				
-
-//				System.out.println("nome : " + rs.getString(1));				
-//				System.out.println("cognome : " + rs.getString(2));				
-//				System.out.println("email : " + rs.getString(3));				
-
-				System.out.println("id : " + rs.getInt("id"));				
-				System.out.println("nome : " + rs.getString("nome"));				
-				System.out.println("cognome : " + rs.getString("cognome"));				
-				System.out.println("email : " + rs.getString("email"));				
-				System.out.println("telefono : " + rs.getString("telefono"));				
-				System.out.println("note : " + rs.getString("note"));				
-
-//				System.out.println("cognome : " + rs.getString("cognome"));				
-//				System.out.println("nome : " + rs.getString("nome"));				
-//				System.out.println("email : " + rs.getString("email"));				
-			}
-
 			
 		} catch (ClassNotFoundException cnfEx) {
 			cnfEx.printStackTrace();
@@ -208,14 +146,59 @@ public class JDBCmanager {
 			sqlEx.printStackTrace();
 		} finally {
 			try {
-				rs.close();
-				statement.close();
+				insertPrepStatement.close();
 				connection.close();
 			} catch (SQLException sqlEx) {
 				sqlEx.printStackTrace();
 			}
 		}
-
+ 	}
+	
+ 	public static void deleteContact(int id) {
+ 		Connection connection = null;
+		PreparedStatement insertPrepStatement = null;
+		
+		try {
+			connection = getConnection();
+			insertPrepStatement = connection.prepareStatement("DELETE FROM rubrica where id = ?");
+			
+			insertPrepStatement.setInt(1, id);
+			insertPrepStatement.executeUpdate();
+			
+		} catch (ClassNotFoundException cnfEx) {
+			cnfEx.printStackTrace();
+		} catch (SQLException sqlEx) {
+			sqlEx.printStackTrace();
+		} finally {
+			try {
+				insertPrepStatement.close();
+				connection.close();
+			} catch (SQLException sqlEx) {
+				sqlEx.printStackTrace();
+			}
+		}
+ 	}
+	//DELETE
+	//statement.executeUpdate("DELETE FROM rubrica where id = 4");
+	
+	public static void main(String[] args) {
+		List<Contatto> contatti = new ArrayList<Contatto>();
+		
+		Contatto contatto = new Contatto();
+		contatto.setNome("Piero");
+		contatto.setCognome("Bianchi");
+		contatto.setEmail("p.bianchi@gmail.com");
+		contatto.setTelefono("3387561234");
+		contatto.setNote("Calcetto");
+		contatti.add(contatto);
+		
+		//insertContacts(contatti);
+		//updateContact(17, contatto);
+		//deleteContact(17);
+		List<Contatto> newContatti = getRubrica();
+		for(Contatto c : newContatti) {
+			System.out.println(c);
+		}
 	}
 
 }
