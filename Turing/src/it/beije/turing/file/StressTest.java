b0873d4da5f9c6caf6bd158ff9f7becb2111d98d
Marco Nardella <marco.nardella97@gmail.com>
2022-04-20 15:00:06 +0200

    Stress test Hibernate

--- Turing/src/it/beije/turing/file/StressTest.java
+++ Turing/src/it/beije/turing/file/StressTest.java
@@ -4,14 +4,25 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
+import it.beije.turing.rubrica.Contatto;
+
 public class StressTest {
 	
 	
 	
 	public static void main(String[] args) {
 		ArrayList<Connection> connections = new ArrayList<>();
+		ArrayList<Session> sessions = new ArrayList<>();
 		int jdbCount = 0;
+		int hbCount = 0;
 		int lastCount = 0;
+		Session session = null;
 		do {
 			
 			try {
@@ -25,9 +36,26 @@ public class StressTest {
 				}
 			} catch (ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
-				System.out.println(jdbCount + "is the max number of connections");
-			}			
-		}while(jdbCount > lastCount);
+				System.out.println(jdbCount + "is the max number of connections for jdbc");
+			}finally {
+				try {
+					
+					Configuration configuration = new Configuration().configure().addAnnotatedClass(Contatto.class);
+					
+					SessionFactory sessionFactory = configuration.buildSessionFactory();
+					
+					sessions.add(sessionFactory.openSession());
+					hbCount++;
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+					System.out.println(hbCount + "is the max number of connections for Hibernate");
+				}
+				
+				
+				
+			}
+		}while(true);
 		
 	}
 
