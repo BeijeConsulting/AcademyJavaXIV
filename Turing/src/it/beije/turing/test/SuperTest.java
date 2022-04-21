0da22994697e896a4696fde0525e410601c6dafc
Marco Nardella <marco.nardella97@gmail.com>
2022-04-21 17:31:09 +0200

    test inheritance

---
+++ Turing/src/it/beije/turing/test/SuperTest.java
@@ -0,0 +1,5 @@
+package it.beije.turing.test;
+
+public class SuperTest {
+
+}
---
+++ Turing/src/it/beije/turing/test/Test.java
@@ -0,0 +1,12 @@
+package it.beije.turing.test;
+
+public class Test extends SuperTest{
+	public static void main(String[] args) {
+		Test test1 = new Test();
+		Test test2 = (Test)new SuperTest();
+		SuperTest test3 = new Test();
+		SuperTest test4 = new SuperTest();
+		
+		test1 = (Test)test3;
+	}
+}
