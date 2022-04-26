279e8ac4163b45468764cfd4ed11fab441913c8c
Ercole24 <ercole24maggio@gmail.com>
2022-04-26 17:16:40 +0200

    Create EntityManagerFactorySingleton.java

---
+++ Turing/src/it/beije/ecommerce/EntityManagerFactorySingleton.java
@@ -0,0 +1,20 @@
+package it.beije.ecommerce;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerFactorySingleton {
+	private static EntityManagerFactory entityManagerFactory;
+	private EntityManagerFactorySingleton() {
+		
+	}
+	
+	public static EntityManager createEntityManager() {
+		if(entityManagerFactory == null) {
+			entityManagerFactory = Persistence.createEntityManagerFactory("corraro_pitossi");
+			
+		}
+		return entityManagerFactory.createEntityManager();
+	}
+}
