481b5e8558e6bfdfcd4e001e3b6986532ee3d7af
simo2898 <70075823+simo2898@users.noreply.github.com>
2022-04-26 17:15:42 +0200

    ShoppingCart Class

---
+++ Turing/src/it/beije/ecommerce/Main.java
@@ -0,0 +1,14 @@
+package it.beije.ecommerce;
+
+import it.beije.turing.ContactsManager.MainManager;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
