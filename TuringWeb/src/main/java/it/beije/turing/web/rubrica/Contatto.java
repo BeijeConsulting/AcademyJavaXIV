b63a285d145251e9f52db4e461c87edbea55eb54
Ercole24 <ercole24maggio@gmail.com>
2022-04-27 14:02:35 +0200

    Rubrica Servlet pt.1

---
+++ TuringWeb/src/main/java/it/beije/turing/web/rubrica/Contatto.java
@@ -0,0 +1,110 @@
+package it.beije.turing.challenge.rubrica;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "rubrica")
+public class Contatto implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4663457290026904954L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cognome")
+	private String cognome;
+	
+	@Column(name = "telefono")
+	private String telefono;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "note")
+	private String note;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder()
+				.append("{ id : ").append(this.id)
+				.append(", cognome : ").append(this.cognome)
+				.append(", nome : ").append(this.nome)
+				.append(", telefono : ").append(this.telefono)
+				.append(", email : ").append(this.email)
+				.append(", note : ").append(this.note).append(" }");
+		
+		return builder.toString();
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contatto other = (Contatto) obj;
+		return Objects.equals(cognome, other.cognome) && Objects.equals(email, other.email) && id == other.id
+				&& Objects.equals(nome, other.nome) && Objects.equals(note, other.note)
+				&& Objects.equals(telefono, other.telefono);
+	}
+}
