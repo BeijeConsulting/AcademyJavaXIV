90a986d239f516ce1838d2d5604521e3a8c0419f
Giuseppe199925 <61455889+Giuseppe199925@users.noreply.github.com>
2022-05-02 16:28:08 +0200

    prova

--- TuringWeb/WebContent/insert_contatto.jsp
+++ TuringWeb/WebContent/insert_contatto.jsp
@@ -1,4 +1,4 @@
-<%@page import="it.beije.turing.web.Contatto"%>
+<%@page import="main.java.it.beije.turing.web.Contatto"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<jsp:useBean id="contatto" class="it.beije.turing.web.Contatto"></jsp:useBean>
+<jsp:useBean id="contatto" class="main.java.it.beije.turing.web.Contatto"></jsp:useBean>
 <%--
 Contatto contatto = (Contatto) session.getAttribute("contatto");
 if (contatto == null) {
@@ -19,10 +19,11 @@ if (contatto == null) {
 --%>
 
 <jsp:setProperty property="nome" name="contatto" param="name"/>
-<%--
-String nome = request.getParameter("name");
-if (nome != null) contatto.setNome(nome);
---%>
+<%
+    System.out.println("entro nella jsp");
+
+%>
+
 <jsp:setProperty property="cognome" name="contatto" param="cognome"/>
 
 NOME : <jsp:getProperty name="contatto" property="nome"/><br>
--- TuringWeb/src/main/java/it/beije/turing/web/MyServlet.java
+++ TuringWeb/src/main/java/it/beije/turing/web/MyServlet.java
@@ -1,24 +1,28 @@
 package main.java.it.beije.turing.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MyServlet
  */
-@WebServlet("/servlet")
+@WebServlet("/test")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private int c=0;
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public MyServlet() {
 		super();
+		System.out.println("apro myservlet");
 		// TODO Auto-generated constructor stub
 	}
 
@@ -28,14 +32,16 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("MyServlet doGet");
-		
-//		Contatto contatto = new Contatto();
-//		contatto.setNome("Pippo");
-//		contatto.setCognome("Rossi");
-//		
-//		request.getSession().setAttribute("contatto", contatto);
-		
-		response.sendRedirect("insert_contatto.jsp");
+
+		Contatto contatto = new Contatto();
+		contatto.setNome("Pippo");
+		contatto.setCognome("Rossi");
+
+		request.getSession().setAttribute("contatto", contatto);
+
+		//response.sendRedirect("insert_contatto.jsp");
+		response.getWriter().append("served nella get: ").append(request.getContextPath());
+
 	}
 
 	/**
@@ -43,8 +49,14 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("enrro");
-		doGet(request, response);
+		System.out.println("Nelle doPost");
+		List<Contatto> sessionContatti= new ArrayList<>();
+		Contatto contatto = new Contatto();
+		contatto.setNome((String) request.getParameter("fname"));
+		contatto.setCognome("Rossi");
+		sessionContatti.add(contatto);
+		HttpSession session = request.getSession();
+		session.setAttribute("fname", contatto.getNome());
+		response.sendRedirect("prova.jsp");
 	}
-
 }
