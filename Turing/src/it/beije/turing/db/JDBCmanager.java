ef3edeb8bd44d4b3506523664c9a6d4f2622a4a5
Lorenzo Orrù <lorenzoorru999@gmail.com>
2022-05-02 15:53:44 +0200

    Update branch

--- Turing/src/it/beije/turing/db/JDBCmanager.java
+++ Turing/src/it/beije/turing/db/JDBCmanager.java
@@ -27,6 +27,8 @@ import it.beije.turing.rubrica.Contatto;
 
 import it.beije.turing.rubrica.Contatto;
 
+import it.beije.turing.rubrica.Contatto;
+
 
 public class JDBCmanager {
 	
@@ -81,6 +83,19 @@ public class JDBCmanager {
 //			insertPrepStatement.setString(5, contatto.getNote());
 //			insertPrepStatement.executeUpdate();
 
+
+//			Contatto contatto = new Contatto();
+//			contatto.setCognome("Leopardi");
+//			contatto.setNome("Giacomo");
+//			contatto.setEmail("g.leopardi@poeti.it");
+//			statement.executeUpdate("INSERT INTO rubrica VALUES (null, '" + contatto.getNome() + "', '" + contatto.getCognome() + "', '" + contatto.getEmail() + "', '" + contatto.getTelefono() + "', " + contatto.getNote() + ")");
+//			insertPrepStatement = connection.prepareStatement("INSERT INTO rubrica VALUES (null, ?, ?, ?, ?, ?)");
+//			insertPrepStatement.setString(1, contatto.getNome());
+//			insertPrepStatement.setString(2, contatto.getCognome());
+//			insertPrepStatement.setString(3, contatto.getEmail());
+//			insertPrepStatement.setString(4, contatto.getTelefono());
+//			insertPrepStatement.setString(5, contatto.getNote());
+//			insertPrepStatement.executeUpdate();
 
 //			Contatto contatto = new Contatto();
 //			contatto.setCognome("Leopardi");
