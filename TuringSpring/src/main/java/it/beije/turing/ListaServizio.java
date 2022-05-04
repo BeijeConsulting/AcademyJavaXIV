5a16b9c595be2fdf99186c5b5c8ff7ab46646912
Marco Nardella <marco.nardella97@gmail.com>
2022-05-04 17:38:26 +0200

    Merging /w TuringSpring

---
+++ TuringSpring/src/main/java/it/beije/turing/ListaServizio.java
@@ -0,0 +1,51 @@
+package it.beije.turing;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lista_servizi")
+public class ListaServizio {
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "struttura_id")
+	private Struttura strutturaId;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "servizio_id")
+	private List<Servizio> servizioId;
+
+	public Struttura getStrutturaId() {
+		return strutturaId;
+	}
+
+	public void setStrutturaId(Struttura strutturaId) {
+		this.strutturaId = strutturaId;
+	}
+
+	public List<Servizio> getServizioId() {
+		return servizioId;
+	}
+
+	public void setServizioId(List<Servizio> servizioId) {
+		this.servizioId = servizioId;
+	}
+
+	public String toString() {
+		StringBuilder builder = new StringBuilder().append("{ Struttura id : ").append(this.strutturaId)
+				.append("{ Servizio id : ").append(this.servizioId);
+		return builder.toString();
+	}
+}
