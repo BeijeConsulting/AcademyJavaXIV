5f98e4433e3cedd56a691ed2d343ad0964ebdce8
GinoGreen <luigiverde97@gmail.com>
2022-05-04 15:39:51 +0200

    server pwd

--- TuringSpring/src/main/java/it/beije/turing/service/FirstService.java
+++ TuringSpring/src/main/java/it/beije/turing/service/FirstService.java
@@ -35,8 +35,8 @@ public class FirstService {
 //		list.add(c2);
 		
 //		List<Contatto> list = contattoRepository.findByCognomeAndNome("Verde", "Piero");
-//		List<Contatto> list = contattoRepository.searchByEmail("m.rossi@beije.it");
-		List<Contatto> list = contattoRepository.findAll();
+		List<Contatto> list = contattoRepository.searchByEmail("m.rossi@beije.it");
+//		List<Contatto> list = contattoRepository.findAll();
 //		for (Contatto c : list) {
 //			System.out.println(c);
 //		}
