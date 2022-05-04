95d9638caa7603dddda8cbf057056a2e88f523c0
simo2898 <70075823+simo2898@users.noreply.github.com>
2022-05-04 17:21:08 +0200

    Team2 Struttura

--- TuringSpring/src/main/java/it/beije/turing/Struttura.java
+++ TuringSpring/src/main/java/it/beije/turing/Struttura.java
@@ -21,11 +21,11 @@ public class Struttura {
     private String descrizione;
 
     @OneToOne(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name="id")
+    @JoinColumn(name="id", referencedColumnName = "host_id")
     private Indirizzo indirizzo;
 
     @OneToOne(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name="id")
+    @JoinColumn(name="host_id")
     private Utente utente;
 
     public Utente getUtente() {
