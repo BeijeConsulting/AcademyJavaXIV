45df4a59e51a8437e2ada6495616e2c5ea712aae
Isghamor <yosci97@gmail.com>
2022-05-05 14:41:02 +0200

    CRUD Regola

--- TuringSpring/src/main/java/it/beije/turing/service/FirstService.java
+++ TuringSpring/src/main/java/it/beije/turing/service/FirstService.java
@@ -5,43 +5,46 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.beije.turing.beans.Regola;
+import it.beije.turing.repository.RegolaRepository;
+
 //import it.beije.turing.example.Contatto;
 //import it.beije.turing.repository.ContattoRepository;
 
 
 @Service
 public class FirstService {
-	
-//	@Autowired
-//	private ContattoRepository contattoRepository;
-	
-	
-//	public List<Contatto> leggiRubrica() {
-//
-//		System.out.println("leggiRubrica..." + this.toString());
-//
-////		Contatto c1 = new Contatto();
-////		c1.setCognome("Rossi");
-////		c1.setNome("Mario");
-////		c1.setTelefono("1234566");
-////
-////		Contatto c2 = new Contatto();
-////		c2.setCognome("Bianchi");
-////		c2.setNome("Marco");
-////		c2.setTelefono("1234568");
-//
-////		List<Contatto> list = new ArrayList<Contatto>();
-////		list.add(c1);
-////		list.add(c2);
-//
-////		List<Contatto> list = contattoRepository.findByCognomeAndNome("Verde", "Piero");
-////		List<Contatto> list = contattoRepository.searchByEmail("m.rossi@beije.it");
-//		List<Contatto> list = contattoRepository.findAll();
-////		for (Contatto c : list) {
-////			System.out.println(c);
-////		}
-//
-//		return list;
-//	}
+
+@Autowired
+RegolaRepository regolaRepository;
+
+ public List<Regola> getRegola()
+ {
+	return regolaRepository.findAll();
+ }
+
+ public Regola newRegola(Regola regola)
+ {
+	 return regolaRepository.save(regola);
+ }
+ 
+ public Regola updateRegola(Regola regola)
+ {
+	 Regola tmp = regolaRepository.findById(regola.getId()).get();
+	 if(regola.getTitolo()!=null&&!"".equals(regola.getTitolo()))
+	 {
+		 tmp.setTitolo(regola.getTitolo());
+	 }
+	 if(regola.getDescrizione()!=null&&!"".equals(regola.getDescrizione()))
+	 {
+		 tmp.setDescrizione(regola.getDescrizione());
+	 }
+	 return regolaRepository.save(tmp);
+ }
+ 
+ public void deleteRegola(Regola regola)
+ {
+	 regolaRepository.deleteById(regola.getId());
+ }
 
 }
