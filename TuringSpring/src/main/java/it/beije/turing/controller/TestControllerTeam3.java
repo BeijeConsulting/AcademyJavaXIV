95109e21c6aeb593ef982fe7943515adcc78a6cd
Isghamor <yosci97@gmail.com>
2022-05-06 17:18:23 +0200

    FIXED

--- TuringSpring/src/main/java/it/beije/turing/controller/TestControllerTeam3.java
+++ TuringSpring/src/main/java/it/beije/turing/controller/TestControllerTeam3.java
@@ -10,7 +10,12 @@ package it.beije.turing.controller;
 	import it.beije.turing.beans.Struttura;
 	import it.beije.turing.repository.AnnuncioRepository;
 	import it.beije.turing.service.FirstService;
-	import it.beije.turing.service.ServiceStrutture;
+import it.beije.turing.service.ServiceAnnuncio;
+import it.beije.turing.service.ServiceFotoAnnuncio;
+import it.beije.turing.service.ServiceListaRegole;
+import it.beije.turing.service.ServicePeriodoPrenotato;
+import it.beije.turing.service.ServiceRegola;
+import it.beije.turing.service.ServiceStrutture;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.stereotype.Controller;
 	import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,105 +28,116 @@ package it.beije.turing.controller;
 	public class TestControllerTeam3 {
 
 	    @Autowired
-	    private FirstService service;
+	    private ServiceAnnuncio serviceann;
+	    
+	    @Autowired
+	    private ServiceFotoAnnuncio servicefotann;
+	    @Autowired
+	    private ServiceRegola servicereg;
+	    @Autowired
+	    private ServiceListaRegole servicelistreg;
+	    @Autowired
+	    private ServicePeriodoPrenotato serviceperiod;
 
 	    @RequestMapping(value = "/team3", method = RequestMethod.GET)
 	    public String loginGet() {
 	        
 	    	
-//	    	System.out.println("ANNUNCIO");
-//	        service.getAnnuncio().forEach(System.out::println);
-//	        Annuncio annuncio = new Annuncio();
-//	        annuncio.setDescrizione("Descrizione");
-//	        annuncio.setNumPostiLetto(5);
-//	        annuncio.setPrezzo(1.0);
-//	        annuncio.setStrutturaId(null);
-//	        annuncio.setTitolo("titolo");
-//	        service.newAnnuncio(annuncio);
-//	        System.out.println("CREAZIONE");
-//	        service.getAnnuncio().forEach(System.out::println);
-//	        System.out.println("MODIFICA");
-//	        annuncio=service.getAnnuncio().get(0);
-//	        annuncio.setDescrizione("MODIFICA!");
-//	        service.updateAnnuncio(annuncio,annuncio.getId());
-//	        service.getAnnuncio().forEach(System.out::println);
-//	        System.out.println("DELETE");
-//	        service.deleteAnnuncio(annuncio.getId());
-//	        service.getAnnuncio().forEach(System.out::println);
+	    	System.out.println("ANNUNCIO");
+	        serviceann.getAnnuncio().forEach(System.out::println);
+	        Annuncio annuncio = new Annuncio();
+	        annuncio.setDescrizione("Descrizione");
+	        annuncio.setNumPostiLetto(5);
+	        annuncio.setPrezzo(1.0);
+	        annuncio.setStrutturaId(null);
+	        annuncio.setTitolo("titolo");
+	        serviceann.newAnnuncio(annuncio);
+	        System.out.println("CREAZIONE");
+	        serviceann.getAnnuncio().forEach(System.out::println);
+	        System.out.println("MODIFICA");
+	        annuncio=serviceann.getAnnuncio().get(0);
+	        annuncio.setDescrizione("MODIFICA!");
+	        serviceann.updateAnnuncio(annuncio,annuncio.getId());
+	        serviceann.getAnnuncio().forEach(System.out::println);
+	        System.out.println("DELETE");
+	        serviceann.deleteAnnuncio(annuncio.getId());
+	        serviceann.getAnnuncio().forEach(System.out::println);
 	        
-//	        System.out.println("FOTOANNUNCIO");
-//	        service.getFotoAnnuncio().forEach(System.out::println);
-//	        FotoAnnuncio annuncio = new FotoAnnuncio();
-//	        annuncio.setAnnuncio(service.getAnnuncio().get(0));
-//	        annuncio.setImmagineId(null);
-//	        service.newFotoAnnuncio(annuncio);
-//	        System.out.println("CREAZIONE");
-//	        service.getFotoAnnuncio().forEach(System.out::println);
-//	        System.out.println("MODIFICA");
-//	        annuncio=service.getFotoAnnuncio().get(0);
-//	        annuncio.setAnnuncio(service.getAnnuncio().get(1));
-//	        service.updateFotoAnnuncio(annuncio,annuncio.getId());
-//	        service.getFotoAnnuncio().forEach(System.out::println);
-//	        System.out.println("DELETE");
-//	        //service.deleteFotoAnnuncio(annuncio.getId());
-//	        service.getFotoAnnuncio().forEach(System.out::println);
+	        System.out.println("FOTOANNUNCIO");
+	        servicefotann.getFotoAnnuncio().forEach(System.out::println);
+	        FotoAnnuncio fotannuncio = new FotoAnnuncio();
+	        fotannuncio.setAnnuncio(serviceann.getAnnuncio().get(0));
+	        fotannuncio.setImmagineId(null);
+	        servicefotann.newFotoAnnuncio(fotannuncio);
+	        System.out.println("CREAZIONE");
+	        servicefotann.getFotoAnnuncio().forEach(System.out::println);
+	        System.out.println("MODIFICA");
+	        fotannuncio=servicefotann.getFotoAnnuncio().get(0);
+	        fotannuncio.setAnnuncio(serviceann.getAnnuncio().get(1));
+	        servicefotann.updateFotoAnnuncio(fotannuncio,fotannuncio.getId());
+	        servicefotann.getFotoAnnuncio().forEach(System.out::println);
+	        System.out.println("DELETE");
+	        //servicefotann.deleteFotoAnnuncio(fotannuncio.getId());
+	        servicefotann.getFotoAnnuncio().forEach(System.out::println);
 	    	
 	    	
 	  	
-//	    	 System.out.println("REGOLA");
-//	         service.getRegola().forEach(System.out::println);
-//	         Regola annuncio = new Regola();
-//	         annuncio.setDescrizione("Descrizione");
-//	         annuncio.setTitolo("Titolo");
-//	         service.newRegola(annuncio);
-//	         System.out.println("CREAZIONE");
-//	         service.getRegola().forEach(System.out::println);
-//	         System.out.println("MODIFICA");
-//	         annuncio=service.getRegola().get(0);
-//	         annuncio.setDescrizione("MODIFICA!");
-//	         service.updateRegola(annuncio);
-//	         service.getRegola().forEach(System.out::println);
-//	         System.out.println("DELETE");
-//	         service.deleteFotoAnnuncio(annuncio.getId());
-//	         service.getRegola().forEach(System.out::println);
+	    	 System.out.println("REGOLA");
+	         servicereg.getRegola().forEach(System.out::println);
+	         Regola regola = new Regola();
+	         regola.setDescrizione("Descrizione");
+	         regola.setTitolo("Titolo");
+	         servicereg.newRegola(regola);
+	         System.out.println("CREAZIONE");
+	         servicereg.getRegola().forEach(System.out::println);
+	         System.out.println("MODIFICA");
+	         regola=servicereg.getRegola().get(0);
+	         regola.setDescrizione("MODIFICA!");
+	         servicereg.updateRegola(regola);
+	         servicereg.getRegola().forEach(System.out::println);
+	         System.out.println("DELETE");
+	         servicereg.deleteRegola(regola);
+	         servicereg.getRegola().forEach(System.out::println);
 	         
-//	       System.out.println("LISTA REGOLE");
-//	       service.getListaRegole().forEach(System.out::println);
-//	       ListaRegole annuncio = new ListaRegole();
-//	       annuncio.setAnnuncioId(service.getAnnuncio().get(0));
-//	       annuncio.setRegolaId(service.getRegola().get(0));
-//	       annuncio.setCompletamento("Completamento");
-//	       service.newListaRegole(annuncio);
-//	       System.out.println("CREAZIONE");
-//	       service.getListaRegole().forEach(System.out::println);
-//	       System.out.println("MODIFICA");
-//	       annuncio=service.getListaRegole().get(0);
-//	       annuncio.setCompletamento("MODIFICA!");
-//	       service.updateListaRegole(annuncio, annuncio.getId());
-//	       service.getListaRegole().forEach(System.out::println);
-//	       System.out.println("DELETE");
-//	       service.deleteListaRegole(annuncio.getId());
-//	       service.getFotoAnnuncio().forEach(System.out::println);
-//	       
-	       System.out.println("PERIODI PRENOTATI");
-	       service.getPeriodiPrenotati().forEach(System.out::println);
-	       PeriodoPrenotato annuncio = new PeriodoPrenotato();
-	       annuncio.setAnnuncio(service.getAnnuncio().get(0));
-	       annuncio.setDataInizio(LocalDate.now());
-	       annuncio.setDataFine(LocalDate.now().plusDays(2));
-	       annuncio.setStatoAccettazione("ACCETTATO");
-	       annuncio.setStatoPagamento("ACCETTATO");
-	       service.newPeriodoPrenotato(annuncio);
+	       System.out.println("LISTA REGOLE");
+	       servicelistreg.getListaRegole().forEach(System.out::println);
+	       ListaRegole listaRegola = new ListaRegole();
+	       listaRegola.setAnnuncioId(serviceann.getAnnuncio().get(0));
+	       listaRegola.setRegolaId(servicereg.getRegola().get(0));
+	       listaRegola.setCompletamento("Completamento");
+	       servicelistreg.newListaRegole(listaRegola);
 	       System.out.println("CREAZIONE");
-	       service.getPeriodiPrenotati().forEach(System.out::println);
+	       servicelistreg.getListaRegole().forEach(System.out::println);
 	       System.out.println("MODIFICA");
-	       annuncio=service.getPeriodiPrenotati().get(0);
-	       annuncio.setDataFine(LocalDate.now().plusYears(1));
-	       service.updatePeriodoPrenotato(annuncio,annuncio.getId());
-	       service.getPeriodiPrenotati().forEach(System.out::println);
+	       listaRegola=servicelistreg.getListaRegole().get(0);
+	       listaRegola.setCompletamento("MODIFICA!");
+	       servicelistreg.updateListaRegole(listaRegola, listaRegola.getId());
+	       servicelistreg.getListaRegole().forEach(System.out::println);
 	       System.out.println("DELETE");
-	       service.deletePeriodoPrenotato(annuncio.getId());
-	       service.getPeriodiPrenotati().forEach(System.out::println);
+	       servicelistreg.deleteListaRegole(listaRegola.getId());
+	       servicelistreg.getListaRegole().forEach(System.out::println);
+	    	
+	    	
+	       
+	       System.out.println("PERIODI PRENOTATI");
+	       serviceperiod.getPeriodiPrenotati().forEach(System.out::println);
+	       PeriodoPrenotato periodo = new PeriodoPrenotato();
+	       periodo.setAnnuncio(serviceann.getAnnuncio().get(0));
+	       periodo.setDataInizio(LocalDate.now());
+	       periodo.setDataFine(LocalDate.now().plusDays(2));
+	       periodo.setStatoAccettazione("ACCETTATO");
+	       periodo.setStatoPagamento("ACCETTATO");
+	       serviceperiod.newPeriodoPrenotato(periodo);
+	       System.out.println("CREAZIONE");
+	       serviceperiod.getPeriodiPrenotati().forEach(System.out::println);
+	       System.out.println("MODIFICA");
+	       periodo=serviceperiod.getPeriodiPrenotati().get(0);
+	       periodo.setDataFine(LocalDate.now().plusYears(1));
+	       serviceperiod.updatePeriodoPrenotato(periodo,periodo.getId());
+	       serviceperiod.getPeriodiPrenotati().forEach(System.out::println);
+	       System.out.println("DELETE");
+	       serviceperiod.deletePeriodoPrenotato(periodo.getId());
+	       serviceperiod.getPeriodiPrenotati().forEach(System.out::println);
 	        
 	        return "index";
 	    }
