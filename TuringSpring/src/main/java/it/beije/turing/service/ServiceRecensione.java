5ec0c789bc2054f1445ad35a0becb7f24966d3a4
Padawan <lorenzo.leopardi95@gmail.com>
2022-05-09 17:15:47 +0200

    REST API recensione

--- TuringSpring/src/main/java/it/beije/turing/service/ServiceRecensione.java
+++ TuringSpring/src/main/java/it/beije/turing/service/ServiceRecensione.java
@@ -4,6 +4,7 @@ import it.beije.turing.beans.Recensione;
 import it.beije.turing.beans.Struttura;
 import it.beije.turing.repository.RecensioneRepository;
 import it.beije.turing.repository.StrutturaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,23 +23,30 @@ public class ServiceRecensione {
         return recensioneRepository.searchByPunteggioRange(min, max);
     }
 
-    public Recensione creaRecensione(String testo,
-                                     String data,
-                                     Integer punteggio,
-                                     Integer reviewerId,
-                                     Integer prenotazioneId){
-        Recensione recensione = new Recensione();
-        recensione.setTesto(testo);
-        recensione.setData(data);
-        recensione.setPunteggio(punteggio);
-        recensione.setReviewerId(reviewerId);
-        recensione.setPrenotazioneId(prenotazioneId);
+    public void creaRecensione(Recensione recensione){
+
         recensioneRepository.saveAndFlush(recensione);
+        return;
+    }
+
+    public Recensione findRecensione (Integer id){
+        Recensione recensione = recensioneRepository.findById(id).get();
         return recensione;
     }
 
     public void rimuoviRecensione (Integer recId){
         recensioneRepository.deleteById(recId);
+    }
+
+
+    public Recensione updateRecensione (Recensione recensione, Integer id){
+        Recensione oldRec = recensioneRepository.findById(id).get();
+        if (oldRec==null){
+            return oldRec;
+        }
+        BeanUtils.copyProperties(recensione, oldRec);
+        recensioneRepository.save(oldRec);
+        return oldRec;
     }
 
     public Recensione updateRecensione (Integer id,
