c6d659d45e7f6c5c7be22d556d5b8b17b18ea083
GinoGreen <luigiverde97@gmail.com>
2022-05-09 17:40:13 +0200

    utente crud json

--- TuringSpring/src/main/java/it/beije/turing/service/ServiceUtente.java
+++ TuringSpring/src/main/java/it/beije/turing/service/ServiceUtente.java
@@ -2,9 +2,11 @@ package it.beije.turing.service;
 
 import it.beije.turing.beans.Utente;
 import it.beije.turing.repository.UtenteRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 
 @Service
@@ -13,27 +15,23 @@ public class ServiceUtente {
 	@Autowired
 	private UtenteRepository utenteRepository;
 
-    public Utente creaUtente(String fname,
-                             String lname,
-                             String email,
-                             String password) {
+    public Utente createUser(Utente user) {
+        System.out.println(user);
 
-        if (fname.isEmpty() ||
-            lname.isEmpty() ||
-            email.isEmpty() ||
-            password.isEmpty()
+        if (user.getNome().isEmpty() ||
+            user.getCognome().isEmpty() ||
+            user.getPassword().isEmpty() ||
+            user.getEmail().isEmpty()
         ) return null;
         else {
 
-            Utente newUtente = new Utente();
-            newUtente.setNome(fname);
-            newUtente.setCognome(lname);
-            newUtente.setEmail(email);
-            newUtente.setPassword(password);
-
-            utenteRepository.saveAndFlush(newUtente);
-            return newUtente;
+            utenteRepository.saveAndFlush(user);
+            return user;
         }
+    }
+
+    public List<Utente> getUsers() {
+        return utenteRepository.findAll();
     }
 
     public Utente getUtenteById(Integer id) {
@@ -44,34 +42,23 @@ public class ServiceUtente {
         return utenteRepository.findDistinctByEmail(email);
     }
 
-    public Utente modificaUtente(String id, String fname, String lname, String email, String password) {
+    public Utente modificaUtente(Integer id, Utente user) {
 
-        Utente utente = getUtenteById(Integer.valueOf(id));
-        if (utente == null) return null;
-        else {
+        Utente old = getUtenteById(id);
 
-            if (!utente.getNome().equals(fname)) {
-                utente.setNome(fname);
-            }
-            if (!utente.getCognome().equals(lname)) {
-                utente.setCognome(lname);
-            }
-            if (!utente.getEmail().equals(email)) {
-                utente.setEmail(email);
-            }
-            if (!utente.getPassword().equals(password)) {
-                utente.setPassword(password);
-            }
+        if (old != null) {
+            BeanUtils.copyProperties(user, old);
+            utenteRepository.saveAndFlush(user);
 
-            utenteRepository.saveAndFlush(utente);
-
-            return utente;
+            return old;
         }
+
+        return null;
 
     }
 
-    public void deleteUtente(Utente utente) {
-        utenteRepository.delete(utente);
+    public void deleteUtente(Integer id) {
+        utenteRepository.deleteById(id);
     }
 
 }
