0f7e140021b8d7cd4bbd4ea67cbbd8fb0222891b
GinoGreen <luigiverde97@gmail.com>
2022-06-13 17:32:12 +0200

    LOGIN REGISTER DONE

--- PopupJS/verde_luigi/script.js
+++ PopupJS/verde_luigi/script.js
@@ -60,9 +60,12 @@ async function showRegisterResult() {
    if (!jsonRegister.hasOwnProperty("message")) {
       app.innerHTML = "";
       app.innerHTML = `<div class="container d-flex justify-content-center align-items-center flex-column">
-                        <h1>Controlla la mail di conferma</h1>
+                        <h1>Registrato con successo</h1>
                         <h2>Che la pace sia con te</h2>
                      </div>`
+      setTimeout(() => {
+         showLoginModal();
+      }, 3000);
    } else {
       const errorMessage = document.getElementById("submit_error");
       errorMessage.textContent = jsonLogin.message;
@@ -93,10 +96,10 @@ async function register() {
    const response = await fetch('https://reservation-api.beije.it/user/registration', {
       method: 'POST',
       body: JSON.stringify({
-         name: fullName[0],
-         surname: fullName[1],
-         email: emailRegister,
-         password: passwordRegister
+         name: fullName[0].trim(),
+         surname: fullName[1].trim(),
+         email: emailRegister.trim(),
+         password: passwordRegister.trim()
       }),
       headers: {
          'Content-type': 'application/json; charset=UTF-8',
@@ -113,6 +116,7 @@ function showLoginModal() {
    app.innerHTML = `<div class="container d-flex justify-content-center align-items-center flex-column">
                      <h1 class="py-3">Accedi</h1>
                      <div id="submit_error" class="d-none alert alert-danger" role="alert"></div>
+                     <div id="invalid_send" class="d-none alert alert-danger" role="alert">Controlla meglio i campi</div>
                      <form id="login" class="w-25">
                         <div class="mb-3">
                            <label for="email" class="form-label">Email address</label>
@@ -146,22 +150,31 @@ function showRegisterModal() {
 
    app.innerHTML = `<div class="container d-flex justify-content-center align-items-center flex-column">
                      <h1 class="py-3">Registrati</h1>
+                     <div id="invalid_send" class="d-none alert alert-danger" role="alert">Controlla meglio i campi</div>
                      <form id="register" class="w-25">				
                         <div class="mb-3">
-                           <label for="name" class="form-label">Nome</label>
+                           <label for="name" class="form-label">Nome e Cognome</label>
                            <input type="name" class="form-control" id="name" name="name">
+                           <p class="invalid-feedback">Nome o cognome non valido</p>
                         </div>
                         <div class="mb-3">
                            <label for="email" class="form-label">Email address</label>
                            <input type="email" class="form-control" id="email" name="email">
+                           <p class="invalid-feedback">Email non valida</p>
                         </div>
                         <div class="mb-3">
                            <label for="email_confirm" class="form-label">Confirm Email address</label>
                            <input type="email" class="form-control" id="email_confirm" name="email_confirm">
+                           <p class="invalid-feedback">Email diversa</p>
                         </div>
                         <div class="mb-3">
                            <label for="password" class="form-label">Password</label>
                            <input type="password" class="form-control" id="password" name="password">
+                           <p class="invalid-feedback">Devi inserire da 8-16 caratteri,<br>
+                              Almeno un carattere speciale,<br>
+                              Almeno una lettera maiuscola e una minuscola,<br>
+                              Almeno un numero<br>
+                           </p>
                         </div>
                         <div class="text-center">
                            <button id="submit_register" type="submit" class="btn btn-light">Registrati</button>
@@ -186,6 +199,12 @@ function showRegisterModal() {
 function addListenerLogin() {
    emailLoginInput.addEventListener("input", (event) => {
       emailLogin = event.target.value;
+      if (!isValidEmail(emailLogin)) {
+         emailLoginInput.classList.add("is-invalid");
+      } else {
+         emailLoginInput.classList.remove("is-invalid");
+         emailLoginInput.classList.add("is-valid");
+      }
    })
    
    passwordLoginInput.addEventListener("input", (event) => {
@@ -199,27 +218,65 @@ function addListenerLogin() {
 
    submitLogin.addEventListener("click", (event) => {
       event.preventDefault();
-      showLoginResult();
+      const fields = [emailLoginInput, passwordLoginInput];
+      let send = true
+      for (const field of fields) {
+         if (field.classList.contains("is-invalid")) {
+            send = false;
+            break;
+         }
+      }
+      
+      if (send) {
+         showLoginResult();
+      } else {
+         const popupInvalid = document.getElementById("invalid_send")
+         popupInvalid.classList.remove("d-none")
+         setTimeout(() => {
+            popupInvalid.classList.add("d-none")
+         }, 3000);
+      }
    })
 }
 
 function addListenerRegister() {
    nameRegisterInput.addEventListener("input", (event) => {
       nameRegister = event.target.value;
+      if (!isValidName()) {
+         nameRegisterInput.classList.add("is-invalid");
+      } else {
+         nameRegisterInput.classList.remove("is-invalid");
+         nameRegisterInput.classList.add("is-valid");
+      }
    })
    
    emailRegisterInput.addEventListener("input", (event) => {
       emailRegister = event.target.value;
+      if (!isValidEmail(emailRegister)) {
+         emailRegisterInput.classList.add("is-invalid");
+      } else {
+         emailRegisterInput.classList.remove("is-invalid");
+         emailRegisterInput.classList.add("is-valid");
+      }
    })
 
    emailConfirmRegisterInput.addEventListener("input", (event) => {
-      if(event.target.value != emailRegister) {
-         console.log("email differnti, controlla meglio");
+      if(event.target.value != emailRegister || !isValidEmail(emailRegister)) {
+         emailConfirmRegisterInput.classList.add("is-invalid");
+      } else {
+         emailConfirmRegisterInput.classList.remove("is-invalid");
+         emailConfirmRegisterInput.classList.add("is-valid");
       }
    })
 
    passwordRegisterInput.addEventListener("input", (event) => {
       passwordRegister = event.target.value;
+      if (!isValidPassword(passwordRegister)) {
+         passwordRegisterInput.classList.add("is-invalid");
+      } else {
+         passwordRegisterInput.classList.remove("is-invalid");
+         passwordRegisterInput.classList.add("is-valid");
+      }
    })
 
    changeForm.addEventListener("click", (event) => {
@@ -229,11 +286,44 @@ function addListenerRegister() {
 
    submitRegister.addEventListener("click", (event) => {
       event.preventDefault();
-      showRegisterResult();
+      const fields = [nameRegisterInput, emailRegisterInput, emailConfirmRegisterInput, passwordRegisterInput];
+      let send = true
+      for (const field of fields) {
+         if (field.classList.contains("is-invalid")) {
+            send = false;
+            break;
+         }
+      }
+      
+      if (send) {
+         showRegisterResult();
+      } else {
+         const popupInvalid = document.getElementById("invalid_send")
+         popupInvalid.classList.remove("d-none")
+         setTimeout(() => {
+            popupInvalid.classList.add("d-none")
+         }, 3000);
+      }
    })
 }
 
 function splitFullName() {
-   console.log(nameRegister.split(" "));
    return nameRegister.split(" ");
+}
+
+function isValidName() {
+   const spaceIndex = nameRegister.trim().indexOf(" ");
+   if (spaceIndex == -1) {
+      return false;
+   }
+   return true;
+}
+
+function isValidEmail(email) {
+   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+
+function isValidPassword(password) {
+   return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/.test(password)
+
 }
