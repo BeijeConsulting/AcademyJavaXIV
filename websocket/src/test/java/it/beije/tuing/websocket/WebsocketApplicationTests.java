83f920174622eb4c52c8a4b59196192fe6371b1c
GinoGreen <luigiverde97@gmail.com>
2022-06-16 16:03:25 +0200

    init

---
+++ websocket/src/test/java/it/beije/tuing/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package it.beije.tuing.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
