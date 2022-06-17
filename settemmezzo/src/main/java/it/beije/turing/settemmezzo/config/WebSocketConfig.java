e9e12b5c018aa06afbff230930f85a3b47fafddb
GinoGreen <luigiverde97@gmail.com>
2022-06-17 14:49:39 +0200

    conf

--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/config/WebSocketConfig.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package it.beije.turing.settemmezzo;
+package it.beije.turing.settemmezzo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
