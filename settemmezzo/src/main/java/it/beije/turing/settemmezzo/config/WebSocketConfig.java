f0f26a14e359577d91f2df5c703ea7d8c5ee7bea
Isghamor <yosci97@gmail.com>
2022-06-17 16:00:15 +0200

    testMethod

--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/config/WebSocketConfig.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/config/WebSocketConfig.java
@@ -13,10 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
+        
     }
 }
--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/testcontroller/TestController.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/testcontroller/TestController.java
@@ -2,6 +2,7 @@ package it.beije.turing.settemmezzo.testcontroller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import it.beije.turing.settemmezzo.config.RegistryContainer;
@@ -14,4 +15,11 @@ public class TestController {
 	public void creaSocket() {
 		 RegistryContainer.getInstance().getReg().addEndpoint("/lol").withSockJS();
 	}
+	
+	@MessageMapping("/hello")
+	@SendTo("/greetings")
+	public String Greeting(String a ) {
+		return("qualcosa"+a);
+	}
+	
 }
