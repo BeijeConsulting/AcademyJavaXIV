3a80b0a7592a911bbe5d6be80eac3cd35cf08046
GinoGreen <luigiverde97@gmail.com>
2022-06-21 09:41:23 +0200

    fix

--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/websocket/config/WebSocketConfig.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/websocket/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/lol").withSockJS();
     }
 }
