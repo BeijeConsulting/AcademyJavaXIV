aefd479c8e88032446e03e46afe54fa79d76549e
ivfly <iv0fly@gmail.com>
2022-06-22 16:37:26 +0200

    SERVER_PORT 5000 per AWS ELB

--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/SettemmezzoApplication.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/SettemmezzoApplication.java
@@ -3,10 +3,14 @@ package it.beije.turing.settemmezzo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SettemmezzoApplication {
 
 	public static void main(String[] args) {
+		log.info("SERVER_PORT : " + System.getenv("SERVER_PORT"));
 		SpringApplication.run(SettemmezzoApplication.class, args);
 	}
 
