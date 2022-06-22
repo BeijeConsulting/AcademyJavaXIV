8678fbbc9916f8c385170708302c1b859879eb7d
ViiRaLe <mattiapagani97@gmail.com>
2022-06-22 17:42:36 +0200

    delete frontend

--- settemmezzo/src/main/resources/static/frontend/src/services/config.js
+++
@@ -1,3 +0,0 @@
-export const BASEURL = '';
-export const TIMEOUT = 1000;
-export const WEBSOCKET = 'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self';
