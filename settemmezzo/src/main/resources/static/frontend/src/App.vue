f6f54a4b5c45efdcd43505bca4b23941a08ec2d1
GinoGreen <luigiverde97@gmail.com>
2022-06-24 09:55:51 +0200

    frontend scaffolding

---
+++ settemmezzo/src/main/resources/static/frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+   <div class="wrapper">
+      <RouterView />
+   </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+
+</style>
---
+++ settemmezzo/src/main/resources/static/frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+   <h1>HOME</h1>
+</template>
+
+<script>
+export default {
+   name: "HomeView"
+}
+</script>
