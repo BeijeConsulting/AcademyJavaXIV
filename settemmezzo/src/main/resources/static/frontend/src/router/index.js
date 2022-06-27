7dcd0702eee26bcd6a9c16d7e1e0e00d21e1509e
GinoGreen <luigiverde97@gmail.com>
2022-06-27 09:11:23 +0200

    match fe

--- settemmezzo/src/main/resources/static/frontend/src/router/index.js
+++ settemmezzo/src/main/resources/static/frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LobbyView from '../views/LobbyView.vue'
+import MatchView from '../views/MatchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/lobby/:lobby',
-      name: 'lobby',
-      component: LobbyView
+      path: '/match',
+      name: 'match',
+      component: MatchView
     }
   ]
 })
