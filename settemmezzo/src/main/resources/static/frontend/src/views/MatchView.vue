c0692ec97e77bf844e9da02e5edc08955af0a7b6
GinoGreen <luigiverde97@gmail.com>
2022-06-27 09:41:56 +0200

    match fe

---
+++ settemmezzo/src/main/resources/static/frontend/src/views/MatchView.vue
@@ -0,0 +1,131 @@
+<template>
+   <div class="container">
+      <div class="match">
+         <h1 class="text-center">MATCH</h1>
+         <div class="match_container">
+            <div class="match_table">
+               <div id="master">
+                  <img src="@/assets/img/logo_official_white.svg" alt="logo beije.it">
+                  <h1>MASTER</h1>
+               </div>
+
+               <div id="player_1" class="player">
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_2" class="player">
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_3" class="player">
+                  <p class="player_name">Marco</p>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_4" class="player">
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_5" class="player">
+                  <p class="player_name">Marco</p>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_6" class="player">
+                  <p class="player_name">Marco</p>
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+               <div id="player_7" class="player">
+                  <div class="match_card">
+                     <div class="back">
+                        <img src="@/assets/img/logo_short_red.svg" alt="logo beije.it">
+                     </div>
+                     <div class="front"></div>
+                  </div>
+               </div>
+               
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "MatchView",
+   
+}
+</script>
