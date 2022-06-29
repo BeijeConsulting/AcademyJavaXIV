d9119562942f7b453970d2ff1c062ceb686f5187
Ercole24 <ercole24maggio@gmail.com>
2022-06-29 12:48:38 +0200

    Update SocketTextHandler.java

--- settemmezzo/src/main/java/it/beije/turing/settemmezzo/websocket/confing2/SocketTextHandler.java
+++ settemmezzo/src/main/java/it/beije/turing/settemmezzo/websocket/confing2/SocketTextHandler.java
@@ -8,16 +8,29 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-@RestController
+@Component
 public class SocketTextHandler extends TextWebSocketHandler {
-
+	
+	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message)
             throws InterruptedException, IOException {
-
-        String payload = message.getPayload();
-        JSONObject jsonObject = new JSONObject(payload);
-        session.sendMessage(new TextMessage("Ciao ERCOLANO @idLobby = " + jsonObject.get("room_id")));
+    	
+    	for(WebSocketSession webSocketSession : sessions) {
+    		if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+    			String payload = message.getPayload();
+    	        JSONObject jsonObject = new JSONObject(payload);
+                webSocketSession.sendMessage(new TextMessage("Ciao ERCOLANO @idLobby = " + jsonObject.get("room_id")));
+            }
+    	}
+    }
+    
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
     }
 }
